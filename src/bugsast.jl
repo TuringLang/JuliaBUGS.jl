147235d5fc0501b7dbbf38f0cd4710a956bcc63a
Philipp Gabler <phipsgabler@users.noreply.github.com>
2022-02-20 11:53:51 +0100

    Better error reporting, fix some bugs (hihi)
    Fixes #8

--- src/bugsast.jl
+++ src/bugsast.jl
@@ -1,3 +1,5 @@
+position_string(l::LineNumberNode) = string(l.file, ":", l.line)
+
 check_lhs(expr) = check_lhs(Bool, expr) || error("Invalid LHS expression `$expr`")
 check_lhs(::Type{Bool}, expr) = false
 check_lhs(::Type{Bool}, ::Symbol) = true
@@ -18,62 +20,69 @@ function bugsast_expression end
 
 Check & normalize BUGS ranges.
 """
-function bugsast_range(expr)
+function bugsast_range(expr, position=LineNumberNode(1, nothing))
     if Meta.isexpr(expr, :(:)) && length(expr.args) in (0, 2)
-        return Expr(:(:), bugsast_expression.(expr.args)...)
+        return Expr(:(:), bugsast_expression.(expr.args, (position,))...)
     elseif Meta.isexpr(expr, :call) && expr.args[1] == :(:) && length(expr.args) in (1, 3)
-        return Expr(:(:), bugsast_expression.(expr.args[2:end])...)
+        return Expr(:(:), bugsast_expression.(expr.args[2:end], (position,))...)
     elseif Meta.isexpr(expr, :$)
         return expr
     else
-        error("Illegal range: `$expr`")
+        error("Illegal range at $(position_string(position)): `$expr`")
     end
 end
 
-function bugsast_index(expr)
+function bugsast_index(expr, position=LineNumberNode(1, nothing))
     try
-        return bugsast_expression(expr)
+        return bugsast_expression(expr, position)
     catch
-        return bugsast_range(expr)
+        return bugsast_range(expr, position)
     end
 end
 
-function bugsast_expression(expr)
+function bugsast_expression(expr, position=LineNumberNode(1, nothing))
     if expr isa Union{Symbol, Number}
         return expr
     elseif Meta.isexpr(expr, :ref)
-        return Expr(:ref, bugsast_index.(expr.args)...)
+        return Expr(:ref, bugsast_index.(expr.args, (position,))...)
     elseif Meta.isexpr(expr, :call)
         if expr.args[1] == :getindex
-            return Expr(:ref, bugsast_index.(expr.args[2:end])...)
+            return Expr(:ref, bugsast_index.(expr.args[2:end], (position,))...)
         elseif expr.args[1] == :truncated || expr.args[1] == :censored
             if length(expr.args) == 4
-                return Expr(expr.args[1], bugsast_expression.(expr.args[2:end])...)
+                return Expr(expr.args[1], bugsast_expression.(expr.args[2:end], (position,))...)
             else
-                error("Illegal $(expr.args[1]) form: $expr")
+                error("Illegal $(expr.args[1]) form at $(position_string(position)): $expr")
             end
         else
-            return Expr(:call, bugsast_expression.(expr.args)...)
+            return Expr(:call, bugsast_expression.(expr.args, (position,))...)
         end
     elseif Meta.isexpr(expr, :block, 2) && expr.args[1] isa LineNumberNode
         # return Expr(:block, expr.args[1], bugsast_expression(expr.args[2]))
-        return bugsast_expression(expr.args[2])
+        return bugsast_expression(expr.args[2], position)
     elseif Meta.isexpr(expr, :$)
         return expr
     else
-        error("Illegal expression: `$expr`")
+        error("Illegal expression at $(position_string(position)): `$expr`")
     end
 end
 
-function bugsast_block(expr)
+"""
+    bugsast_statement(expr)
+
+Check & normalize BUGS blocks, i.e., bodies of `if` and `for` statements.
+
+`LineNumberNode`s are removed, the remaining expressions are checked as statements.
+"""
+function bugsast_block(expr, position=LineNumberNode(1, nothing))
     if Meta.isexpr(expr, :block)
-        stmts = [bugsast_statement(e) for e in expr.args if !(e isa LineNumberNode)]
+        stmts = [bugsast_statement(e, position) for e in expr.args if !(e isa LineNumberNode)]
         return Expr(:block, stmts...)
     else
         try
-            return Expr(:block, bugsast_statement(expr))
+            return Expr(:block, bugsast_statement(expr, position))
         catch
-            error("Expression `$expr` is not a block")
+            error("Expression `$expr` at $(position_string(position)) is not a block")
         end
     end
 end
@@ -83,36 +92,36 @@ end
 
 Check & normalize BUGS statements (logical & stochastic assignment, for, if).
 """
-function bugsast_statement(expr::Expr)
+function bugsast_statement(expr::Expr, position=LineNumberNode(1, nothing))
     if Meta.isexpr(expr, :(=), 2)
-        lhs, rhs = bugsast_expression.(expr.args)
+        lhs, rhs = bugsast_expression.(expr.args, (position,))
         check_lhs(lhs)
         return Expr(:(=), lhs, rhs)
     elseif Meta.isexpr(expr, :(~), 2)
-        lhs, rhs = bugsast_expression.(expr.args)
+        lhs, rhs = bugsast_expression.(expr.args, (position,))
         check_lhs(lhs)
         return Expr(:(~), lhs, rhs)
     elseif Meta.isexpr(expr, :if, 2)
         condition, body = expr.args
-        return Expr(:if, bugsast_expression(condition), bugsast_block(body))
+        return Expr(:if, bugsast_expression(condition, position), bugsast_block(body, position))
     elseif Meta.isexpr(expr, :for, 2)
         condition, body = expr.args
         if Meta.isexpr(condition, :(=), 2)
             var = condition.args[1]
-            range = bugsast_range(condition.args[2])
+            range = bugsast_range(condition.args[2], position)
             if !(var isa Symbol)
-                error("Illegal loop variable declaration: `$condition`")
+                error("Illegal loop variable declaration at $(position_string(position)): `$condition`")
             else
                 condition = Expr(:(=), var, range)
-                return Expr(:for, condition, bugsast_block(body))
+                return Expr(:for, condition, bugsast_block(body, position))
             end
         else
-            error("Invalid loop header: `$condition`")
+            error("Invalid loop header at $(position_string(position)): `$condition`")
         end
     elseif Meta.isexpr(expr, :call, 3) && expr.args[1] == :(~)
-        return bugsast_statement(Expr(:(~), expr.args[2:end]...))
+        return bugsast_statement(Expr(:(~), expr.args[2:end]...), position)
     elseif Meta.isexpr(expr, :block)
-        return bugsast_block(expr)
+        return bugsast_block(expr, position)
     elseif Meta.isexpr(expr, :$)
         return expr
     else
@@ -120,8 +129,8 @@ function bugsast_statement(expr::Expr)
     end
 end
 
-function bugsast(expr)
-    return bugsast_block(expr)
+function bugsast(expr, position=LineNumberNode(1, nothing))
+    return bugsast_block(expr, position)
 end
 
 """
@@ -134,20 +143,22 @@ Used expression heads: `:~` for tilde calls, `:ref` for indexing, `:(:)` for ran
 converted from `:call` variants.
 """
 macro bugsast(expr)
-    return Meta.quot(bugsast(expr))
+    return Meta.quot(bugsast(expr, __source__))
 end
 
 
-macro bugsmodel_str(s)
+function bugs_to_julia(s)
     # remove parentheses around loops
-    transformed_code = replace(s, r"for\p{Zs}*\((.*)\)\p{Zs}*{" => s"for \1 {")
-    transformed_code = replace(
-        transformed_code,
+    s = replace(s, r"for\p{Zs}*\((.*)\)\p{Zs}*{" => s"for \1 {")
+    
+    s = replace(
+        s,
         "<-" => "=",
         # blocks in if and for replaced by respective delimiters (; ≃ \n)
         "{" => ";",
         "}" => "end",
         # empty slices (with lookahead to replace multiple in a series)
+        r"\[\p{Zs}*\]" => "[:]",
         r"\[\p{Zs}*(?=,)" => "[:",
         r",\p{Zs}*(?=[,\]])" => ",:",
         # ignore reserved words (\b is word boundary)
@@ -157,8 +168,11 @@ macro bugsmodel_str(s)
         # wrap variable names in var-strings (to allow variable names with .)
         r"((?:(?:\p{L}\p{M}*)|\.)(?:(?:\p{L}\p{M}*)|\.|\p{N})*)" => s"var\"\1\"", 
     )
-    transformed_code = replace(
-        transformed_code,
+
+    # special censoring/truncation syntax is converted to function calls, with `nothing`
+    # inserted for left-out bounds
+    s = replace(
+        s,
         r"(var\"[^\"]+\"\([^~<=]*\))\p{Zs}*T\p{Zs}*\(\p{Zs}*,(.+)\)" => s"truncated(\1, nothing, \2)",
         r"(var\"[^\"]+\"\([^~<=]*\))\p{Zs}*T\p{Zs}*\((.+),\p{Zs}*\)" => s"truncated(\1, \2, nothing)",
         r"(var\"[^\"]+\"\([^~<=]*\))\p{Zs}*T\p{Zs}*\((.+),(.+)\)" => s"truncated(\1, \2, \3)",
@@ -166,10 +180,24 @@ macro bugsmodel_str(s)
         r"(var\"[^\"]+\"\([^~<=]*\))\p{Zs}*C\p{Zs}*\((.+),\p{Zs}*\)" => s"censored(\1, \2, nothing)",
         r"(var\"[^\"]+\"\([^~<=]*\))\p{Zs}*C\p{Zs}*\((.+),(.+)\)" => s"censored(\1, \2, \3)",
     )
-    # wrap the whole thing in a block
-    transformed_code = "begin\n$transformed_code\nend"
-    # println(transformed_code)
-    
-    expr = Meta.parse(transformed_code)
-    return Meta.quot(bugsast(expr))
+
+    return s
 end
+
+macro bugsmodel_str(s::String)
+    # Convert and wrap the whole thing in a block for parsing
+    transformed_code = "begin\n$(bugs_to_julia(s))\nend"
+    try
+        expr = Meta.parse(transformed_code)
+        return Meta.quot(bugsast(expr, __source__))
+    catch e
+        if e isa Base.Meta.ParseError
+            new_msg = replace(e.msg, "none:1" => position_string(__source__))
+            rethrow(ErrorException(new_msg))
+        else
+            rethrow()
+        end
+    end
+end
+
+
