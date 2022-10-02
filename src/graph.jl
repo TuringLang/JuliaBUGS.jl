675a786bdc11adf46b8250cb8fbfc62cdb9feada
Xianda Sun <sunxdt@gmail.com>
2022-10-02 04:43:24 -0700

    Add native graph rep; still have logical nodes.

---
+++ src/graph.jl
@@ -0,0 +1,62 @@
+using Graphs
+
+struct BUGSGraph
+    nodeenum::Dict{Symbol, Int32}
+    reversenodeenum::Dict{Int32, Symbol}
+    dag::DiGraph
+    nodetype::Dict{Int32, Symbol}
+    observevalues::Dict{Int32, Real}
+    mutablevalues::Dict{Int32, Real}
+    nodefunc::Dict{Int32, Function}
+    sortednode::Vector{Int32}
+end
+
+function BUGSGraph(tograph::Dict{Any, Any})
+    nodeenum = Dict{Symbol, Int32}()
+    for (i, k) in enumerate(keys(tograph))
+        nodeenum[k] = i
+    end
+    reversenodeenum = Dict{Int32, Symbol}(v => k for (k, v) in nodeenum)
+
+    parents = Dict{Symbol, Vector}()
+    for k in keys(tograph)
+        parents[k] = tograph[k][2].args[1].args
+    end
+
+    DAG = DiGraph(length(keys(tograph)))
+    for k in keys(tograph)
+        for p in parents[k]
+            add_edge!(DAG, nodeenum[p], nodeenum[k])
+        end
+    end
+
+    nodetypes = Dict{Int32, Symbol}()
+    for k in keys(tograph)
+        nodetypes[nodeenum[k]] = tograph[k][3]
+    end
+
+    observevalues = Dict{Int32, Real}()
+    for k in keys(tograph)
+        if tograph[k][3] == :Observations
+            observevalues[nodeenum[k]] = tograph[k][1]
+        end
+    end
+
+    mutablevalues = Dict{Int32, Real}()
+    for k in keys(tograph)
+        if tograph[k][3] != :Observations
+            mutablevalues[nodeenum[k]] = tograph[k][1]
+        end
+    end
+
+    nodefunc = Dict{Int32, Function}()
+    for k in keys(tograph)
+        nodefunc[nodeenum[k]] = eval(tograph[k][2])
+    end
+
+    sortednode = topological_sort_by_dfs(DAG)
+
+    return BUGSGraph(nodeenum, reversenodeenum, DAG, nodetypes, observevalues, mutablevalues, nodefunc, sortednode)
+end
+
+
