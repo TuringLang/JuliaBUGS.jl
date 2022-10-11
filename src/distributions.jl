b0d71ab2012a40acc0bbb8f41144034674c0d96b
Xianda Sun <sunxdt@gmail.com>
2022-10-11 08:44:42 -0700

    Replace dependency on Turing with DynamicPPL.

--- src/distributions.jl
+++ src/distributions.jl
@@ -1,7 +1,31 @@
-# TODO: import Turing's Flat definition now, later we should move Flat definition here and import the definition in Turing
-using Turing: Flat
+# Support for distributions that are part of BUGS but not implemented in Distributions.jl.
 
-Distributions.cdf(d::Flat, x::Real) = 0
+# Modified from https://github.com/TuringLang/Turing.jl/blob/master/src/stdlib/distributions.jl
+# Rename `Flat` to `SPPLFlat` to avoid name conflict with Turing.jl
+"""
+    Flat
+
+The *flat distribution* is the improper distribution of real numbers that has the improper
+probability density function
+```math
+f(x) = 1.
+```
+"""
+struct SPPLFlat <: ContinuousUnivariateDistribution end
+
+Base.minimum(::SPPLFlat) = -Inf
+Base.maximum(::SPPLFlat) = Inf
+
+Base.rand(rng::Random.AbstractRNG, d::SPPLFlat) = rand(rng)
+Distributions.logpdf(::SPPLFlat, x::Real) = zero(x)
+
+# TODO: only implement `logpdf(d, ::Real)` if support for Distributions < 0.24 is dropped
+Distributions.pdf(d::SPPLFlat, x::Real) = exp(logpdf(d, x))
+Distributions.cdf(d::SPPLFlat, x::Real) = 0
+
+# For vec support
+Distributions.logpdf(::SPPLFlat, x::AbstractVector{<:Real}) = zero(x)
+Distributions.loglikelihood(::SPPLFlat, x::AbstractVector{<:Real}) = zero(eltype(x))
 
 """
     LeftTruncatedFlat
@@ -62,17 +86,17 @@ function Distributions.loglikelihood(d::RightTruncatedFlat, x::AbstractVector{<:
     return any(xi >= upper for xi in x) ? T(Inf) : zero(T)
 end
 
-function truncated(d::Flat, l::Real, r::Real)
+function truncated(d::SPPLFlat, l::Real, r::Real)
     if l > r
         throw(ArgumentError("invalid truncation interval: $l > $r"))
     end
     return Uniform(l, r)
 end
 
-function truncated(d::Flat, l::Real, ::Nothing)
+function truncated(d::SPPLFlat, l::Real, ::Nothing)
     return LeftTruncatedFlat(l)
 end
 
-function truncated(d::Flat, ::Nothing, r::Real)
+function truncated(d::SPPLFlat, ::Nothing, r::Real)
     return RightTruncatedFlat(r)
 end
