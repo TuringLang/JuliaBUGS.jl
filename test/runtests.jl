f2973131650ec3b89a55968ac7906233b29dee91
Hong Ge <3279477+yebai@users.noreply.github.com>
2023-02-24 20:22:14 +0000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,12 @@
-using SymbolicPPL
-using Symbolics
 using Test
 using MacroTools: @q
+using JuliaBUGS
+using Symbolics
 
 include("bugsast.jl")
 include("bugsmodel.jl")
 include("compiler.jl")
 
-using JuliaBUGS
 
 @testset "JuliaBUGS.jl" begin
     # Write your tests here.
