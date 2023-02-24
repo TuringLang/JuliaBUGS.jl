063dcb543ca3a8b6908ba87029e12af68d112128
Hong Ge <hg344@cam.ac.uk>
2023-02-24 20:52:31 +0000

    fix import

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,8 @@ using Symbolics
 
 include("bugsast.jl")
 include("bugsmodel.jl")
-include("compiler.jl")
+# include("compiler.jl")
+include("compiler_passes.jl")
 
 @testset "JuliaBUGS.jl" begin
     # Write your tests here.
