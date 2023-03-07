ad5a34da8396b5dba0812cf82087ee96be55a0aa
Xianda Sun <5433119+sunxd3@users.noreply.github.com>
2023-03-07 06:20:34 -0800

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/utils.jl
+++ src/utils.jl
@@ -29,10 +29,19 @@ function test_BUGS_model_with_default_hmc(model_def, data, init)
 
     initial_ϵ = find_good_stepsize(hamiltonian, initial_θ)
     integrator = Leapfrog(initial_ϵ)
-    proposal = NUTS{MultinomialTS, GeneralisedNoUTurn}(integrator)
+    proposal = NUTS{MultinomialTS,GeneralisedNoUTurn}(integrator)
     adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(0.8, integrator))
 
-    samples, stats = sample(hamiltonian, proposal, initial_θ, n_samples, adaptor, n_adapts; drop_warmup=true, progress=true);
+    samples, stats = sample(
+        hamiltonian,
+        proposal,
+        initial_θ,
+        n_samples,
+        adaptor,
+        n_adapts;
+        drop_warmup=true,
+        progress=true,
+    )
     traces = [JuliaBUGS.transform_samples(p, sample) for sample in samples]
     open("/home/sunxd/JuliaBUGS.jl/notebooks/output.jl", "w+") do f
         for k in p.parameters
@@ -44,13 +53,13 @@ function test_BUGS_model_with_default_hmc(model_def, data, init)
     end
 end
 
-function test_BUGS_model_with_default_mh(model_def, data, init)    
+function test_BUGS_model_with_default_mh(model_def, data, init)
     p = compile(model_def, data, init)
     initial_θ = JuliaBUGS.gen_init_params(p)
 
     D = LogDensityProblems.dimension(p)
     spl = RWMH(MvNormal(zeros(D), I))
-    samples = sample(p, spl, 100000);
+    samples = sample(p, spl, 100000)
 
     traces = [JuliaBUGS.transform_samples(p, sample.params) for sample in samples]
     open("/home/sunxd/JuliaBUGS.jl/notebooks/output.jl", "w+") do f
