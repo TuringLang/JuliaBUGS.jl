53acef40c615e87388191eda3cc8db54515a93bf
Xianda Sun <sunxdt@gmail.com>
2023-08-07 14:45:03 +0100

    formatting

--- test/parser.jl
+++ test/parser.jl
@@ -418,7 +418,8 @@ model
        tau[6] <- sqrt(tau.sqrd[6])
        inv.tau.sqrd[6] <- 1 / tau.sqrd[6]
     }
-    """)
+    """,
+    )
 
     # salm
     parse_bugs("""
@@ -2202,36 +2203,38 @@ model
 
     # Lotka-Volterra
     # ! currently error, `D` function is not defined, probably won't impelment 
-    @test_throws ErrorException parse_bugs("""
-        model
-        {
-        solution[1:ngrid, 1:ndim] <- ode.solution(init[1:ndim], tgrid[1:ngrid], D(C[1:ndim], t),
-        origin, tol)
+    @test_throws ErrorException parse_bugs(
+        """
+    model
+    {
+    solution[1:ngrid, 1:ndim] <- ode.solution(init[1:ndim], tgrid[1:ngrid], D(C[1:ndim], t),
+    origin, tol)
 
-        alpha <- exp(log.alpha)
-        beta <- exp(log.beta)
-        gamma <- exp(log.gamma)
-        delta <- exp(log.delta)
-        log.alpha ~ dnorm(0.0, 0.0001)
-        log.beta ~ dnorm(0.0, 0.0001)
-        log.gamma ~ dnorm(0.0, 0.0001)
-        log.delta ~ dnorm(0.0, 0.0001)
+    alpha <- exp(log.alpha)
+    beta <- exp(log.beta)
+    gamma <- exp(log.gamma)
+    delta <- exp(log.delta)
+    log.alpha ~ dnorm(0.0, 0.0001)
+    log.beta ~ dnorm(0.0, 0.0001)
+    log.gamma ~ dnorm(0.0, 0.0001)
+    log.delta ~ dnorm(0.0, 0.0001)
 
-        D(C[1], t) <- C[1] * (alpha - beta * C[2])
-        D(C[2], t) <- -C[2] * (gamma - delta * C[1])
-            
-        for (i in 1:ngrid)
-        {
-        sol_x[i] <- solution[i, 1]
-        obs_x[i] ~ dnorm(sol_x[i], tau.x)
-        sol_y[i] <- solution[i, 2]
-        obs_y[i] ~ dnorm(sol_y[i], tau.y)
-        }
-            
-        tau.x ~ dgamma(a, b)
-        tau.y ~ dgamma(a, b)
-        }
-    """)
+    D(C[1], t) <- C[1] * (alpha - beta * C[2])
+    D(C[2], t) <- -C[2] * (gamma - delta * C[1])
+        
+    for (i in 1:ngrid)
+    {
+    sol_x[i] <- solution[i, 1]
+    obs_x[i] ~ dnorm(sol_x[i], tau.x)
+    sol_y[i] <- solution[i, 2]
+    obs_y[i] ~ dnorm(sol_y[i], tau.y)
+    }
+        
+    tau.x ~ dgamma(a, b)
+    tau.y ~ dgamma(a, b)
+    }
+"""
+    )
 
     # Five compartments
     # Change points
