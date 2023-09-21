40546c7250ccd6a4f9f45ddf585a287ad3467eb4
Xianda Sun <sunxdt@gmail.com>
2023-09-21 08:41:12 +0100

    Add kwargs pass through

--- ext/JuliaBUGSGraphMakieExt.jl
+++ ext/JuliaBUGSGraphMakieExt.jl
@@ -5,10 +5,10 @@ using GraphMakie
 using JuliaBUGS
 using JuliaBUGS.MetaGraphsNext
 
-function GraphMakie.graphplot(m::JuliaBUGS.BUGSModel)
-    return graphplot(m.g, m.parameters)
+function GraphMakie.graphplot(m::JuliaBUGS.BUGSModel; kwargs...)
+    return graphplot(m.g, m.parameters; kwargs...)
 end
-function GraphMakie.graphplot(g::JuliaBUGS.BUGSGraph, parameters)
+function GraphMakie.graphplot(g::JuliaBUGS.BUGSGraph, parameters; kwargs...)
     colors = []
     for node in labels(g)
         if g[node].node_type == JuliaBUGS.Stochastic
@@ -21,8 +21,10 @@ function GraphMakie.graphplot(g::JuliaBUGS.BUGSGraph, parameters)
             push!(colors, :red)
         end
     end
+    ilabels = get(kwargs, :ilabels, map(x -> String(Symbol(x)), labels(g)))
+    node_color = get(kwargs, :node_color, colors)
     return graphplot(
-        g.graph; ilabels=map(x -> String(Symbol(x)), labels(g)), node_color=colors
+        g.graph; ilabels=ilabels, node_color=node_color, kwargs...
     )
 end
 
--- ext/JuliaBUGSTikzGraphsExt.jl
+++ ext/JuliaBUGSTikzGraphsExt.jl
@@ -4,12 +4,13 @@ using TikzGraphs
 using JuliaBUGS
 using JuliaBUGS.MetaGraphsNext
 
-function TikzGraphs.plot(m::JuliaBUGS.BUGSModel)
-    return TikzGraphs.plot(m.g, m.parameters)
+function TikzGraphs.plot(m::JuliaBUGS.BUGSModel; kwargs...)
+    return TikzGraphs.plot(m.g, m.parameters; kwargs...)
 end
-function TikzGraphs.plot(graph::JuliaBUGS.BUGSGraph, parameters)
+
+function TikzGraphs.plot(g::JuliaBUGS.BUGSGraph, parameters; kwargs...)
     color_dict = Dict{Int,String}()
-    for (i, node) in enumerate(labels(graph))
+    for (i, node) in enumerate(labels(g))
         if node in parameters
             color_dict[i] = "fill=green!10"
         else
@@ -17,12 +18,17 @@ function TikzGraphs.plot(graph::JuliaBUGS.BUGSGraph, parameters)
         end
     end
 
+    node_style = get(kwargs, :node_style, "draw, rounded corners, fill=blue!10")
+    node_styles = get(kwargs, :node_styles, color_dict)
+    edge_style = get(kwargs, :edge_style, "black")
+
     return TikzGraphs.plot(
-        graph.graph,
-        map(x -> String(Symbol(x)), labels(graph));
-        node_style="draw, rounded corners, fill=blue!10",
-        node_styles=color_dict,
-        edge_style="black",
+        g.graph,
+        map(x -> String(Symbol(x)), labels(g));
+        node_style=node_style,
+        node_styles=node_styles,
+        edge_style=edge_style,
+        kwargs...
     )
 end
 
