fb699ddba019b877514fe33e6f88aaf2d20cdcfc
Xianda Sun <sunxdt@gmail.com>
2023-10-12 09:06:34 +0100

    Move tests to a separate file

--- test/inference.jl
+++ test/inference.jl
@@ -1,17 +1,3 @@
-# Tests for `getparams`, using `Rats` model
-@testset "`getparams` with Rats" begin
-    m = :rats
-    data = JuliaBUGS.BUGSExamples.VOLUME_I[m].data
-    inits = JuliaBUGS.BUGSExamples.VOLUME_I[m].inits[1]
-    model = JuliaBUGS.compile(JuliaBUGS.BUGSExamples.VOLUME_I[m].model_def, data, inits)
-    model_notran = JuliaBUGS.settrans(model, false)
-
-    @test LogDensityProblems.logdensity(model, JuliaBUGS.getparams(model)) ==
-        JuliaBUGS.evaluate!!(model)[2]
-    @test LogDensityProblems.logdensity(model_notran, JuliaBUGS.getparams(model_notran)) ==
-        JuliaBUGS.evaluate!!(model_notran)[2]
-end
-
 # ReverseDiff
 
 @testset "trans-dim bijectors tape compilation" begin
--- test/runtests.jl
+++ test/runtests.jl
@@ -100,6 +100,10 @@ end
     model = compile(model_def, data, inits)
 end
 
+@testset "Utils" begin
+    include("utils.jl")
+end
+
 @testset "Log Probability Test" begin
     include("run_logp_tests.jl")
     @testset "Single stochastic variable test" begin
@@ -117,4 +121,3 @@ end
 @testset "Markov Blanket" begin
     include("graphs.jl")
 end
-
