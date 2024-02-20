0ffe3be9f1fe68ec3e57360ec0c35e937c2c2a6e
Xianda Sun <sunxdt@gmail.com>
2024-02-20 05:40:02 +0000

    better names

--- src/metaprogramming/utils.jl
+++ src/metaprogramming/utils.jl
@@ -1,36 +1,40 @@
 """
-    extract_variable_names(expr, excluded)
+    extract_variable_names_and_numdims(expr, excluded)
 
-Return a NamedTuple mapping variable names to their dimensionality.
+Extract all the array variable names and number of dimensions from a given simple expression. 
 
 # Examples:
 ```jldoctest
-julia> extract_variable_names(:((a + b) c):((a + b) c), ())
+julia> extract_variable_names_and_numdims(:((a + b) * c), ())
 (a = 0, b = 0, c = 0)
 
-julia> extract_variable_names(:((a + b) c):((a + b) c), (:a,))
+julia> extract_variable_names_and_numdims(:((a + b) * c), (:a,))
 (b = 0, c = 0)
 
-julia> extract_variable_names(:(a[i]), ())
+julia> extract_variable_names_and_numdims(:(a[i]), ())
 (a = 1, i = 0)
 
-julia> extract_variable_names(:(a[i]), (:i,))
+julia> extract_variable_names_and_numdims(:(a[i]), (:i,))
 (a = 1,)
 
-julia> extract_variable_names(42, ())
+julia> extract_variable_names_and_numdims(42, ())
 NamedTuple()
 
-julia> extract_variable_names(:x, (:x,))
+julia> extract_variable_names_and_numdims(:x, (:x,))
 NamedTuple()
 ```
 """
-function extract_variable_names(::Union{Int,Float64}, ::Tuple{Vararg{Symbol}})
+function extract_variable_names_and_numdims(::Union{Int,Float64}, ::Tuple{Vararg{Symbol}})
     return (;)
 end
-function extract_variable_names(expr::Symbol, excluded::Tuple{Vararg{Symbol}})
-    return expr in excluded || expr in (:missing, :nothing) ? (;) : NamedTuple{(expr,)}((0,))
+function extract_variable_names_and_numdims(expr::Symbol, excluded::Tuple{Vararg{Symbol}})
+    return if expr in excluded || expr in (:missing, :nothing)
+        (;)
+    else
+        NamedTuple{(expr,)}((0,))
+    end
 end
-function extract_variable_names(expr::Expr, excluded::Tuple{Vararg{Symbol}})
+function extract_variable_names_and_numdims(expr::Expr, excluded::Tuple{Vararg{Symbol}})
     variables = Dict{Symbol,Int}()
     MacroTools.prewalk(expr) do sub_expr
         if !(sub_expr isa Expr)
@@ -56,17 +60,17 @@ function extract_variable_names(expr::Expr, excluded::Tuple{Vararg{Symbol}})
 end
 
 """
-    extract_array_ndims(expr::Expr)
+    extract_variable_names_and_numdims(expr::Expr)
    
 Extract all the array variable names and number of dimensions.
 
 # Example:
 ```jldoctest
-julia> extract_array_ndims(BUGSExamples.leuk.model_def)
+julia> extract_variable_names_and_numdims(BUGSExamples.leuk.model_def)
 (N = 0, T = 0, Y = 2, var"obs.t" = 1, eps = 0, t = 1, dN = 2, fail = 1, Idt = 2, Z = 1, beta = 0, dL0 = 1, mu = 1, c = 0, var"dL0.star" = 1, var"S.treat" = 1, var"S.placebo" = 1, r = 0)
 ```
 """
-function extract_array_ndims(expr::Expr)
+function extract_variable_names_and_numdims(expr::Expr)
     return extract_array_ndims_block(expr, (), NamedTuple())
 end
 
@@ -108,7 +112,7 @@ function extract_array_ndims_expr(
     loop_vars::Tuple{Vararg{Symbol}},
     array_ndims::NamedTuple{Ns,Ts},
 ) where {Ns,Ts}
-    variables = extract_variable_names(expr, loop_vars)
+    variables = extract_variable_names_and_numdims(expr, loop_vars)
     for var in intersect(keys(variables), Ns)
         if variables[var] !== array_ndims[var]
             error("Variable $var has inconsistent dimensions")
@@ -118,35 +122,34 @@ function extract_array_ndims_expr(
 end
 
 """
-    extract_all_vars(expr::Expr)
+    extract_variable_names(expr::Expr)
 
 Extract all the variable names used in the program.
 # Example:
 ```jldoctest
-julia> extract_all_vars(BUGSExamples.leuk.model_def)
+julia> extract_variable_names(BUGSExamples.leuk.model_def)
 (:N, :T, :Y, Symbol("obs.t"), :eps, :t, :dN, :fail, :Idt, :Z, :beta, :dL0, :mu, :c, Symbol("dL0.star"), Symbol("S.treat"), Symbol("S.placebo"), :r)
 ```
 """
-function extract_all_vars(expr::Expr)
-    return Tuple(keys(extract_array_ndims(expr)))
+function extract_variable_names(expr::Expr)
+    return Tuple(keys(extract_variable_names_and_numdims(expr)))
 end
 
 """
-    extract_variables_used_in_bounds_and_indices(expr::Expr)
+    extract_variables_in_bounds_and_lhs_indices(expr::Expr)
 
 Extract all the variable names used in the bounds and indices of the arrays in the program.
 
 # Example:
 ```jldoctest
-julia> JuliaBUGS.extract_variables_used_in_bounds_and_indices(model_def)
+julia> JuliaBUGS.extract_variables_in_bounds_and_lhs_indices(BUGSExamples.leuk.model_def)
 (:N, :T)
 ```
 """
-function extract_variables_used_in_bounds_and_indices(expr::Expr)
-    return extract_variables_used_in_bounds_and_lhs_indices_block(expr, (), ())
+function extract_variables_in_bounds_and_lhs_indices(expr::Expr)
+    return extract_variables_in_bounds_and_lhs_indices(expr, (), ())
 end
-
-function extract_variables_used_in_bounds_and_lhs_indices_block(
+function extract_variables_in_bounds_and_lhs_indices(
     expr::Expr, loop_vars::Tuple{Vararg{Symbol}}, variables::Tuple{Vararg{Symbol}}
 )
     for stmt in expr.args
@@ -162,7 +165,7 @@ function extract_variables_used_in_bounds_and_lhs_indices_block(
                 body_
             end
         )
-            variables = extract_variables_used_in_bounds_and_lhs_indices_block(
+            variables = extract_variables_in_bounds_and_lhs_indices(
                 body,
                 (loop_vars..., loop_var),
                 extract_variables_used_in_expr(
@@ -176,26 +179,12 @@ function extract_variables_used_in_bounds_and_lhs_indices_block(
     return variables
 end
 
-"""
-    extract_variables_used_in_expr(expr::Union{Int,Float64,Symbol,Expr}, loop_vars::Tuple{Vararg{Symbol}}, variables::Tuple{Vararg{Symbol}})
-    
-Extract all the variable names used in expression, excluding the ones in `loop_vars`, and combined with those of `variables`.
-
-# Example:
-```jldoctest
-julia> JuliaBUGS.extract_variables_used_in_expr(:(a + b[1] + b[c[1], c[d]]), (), ())
-(:a, :b, :d, :c)
-
-julia> JuliaBUGS.extract_variables_used_in_expr(:(a + b[1] + b[c[1], c[d]]), (:d,), (:e,))
-(:e, :a, :b, :c)
-```
-"""
 function extract_variables_used_in_expr(
     expr::Union{Int,Float64,Symbol,Expr},
     loop_vars::Tuple{Vararg{Symbol}},
     variables::Tuple{Vararg{Symbol}},
 )::Tuple{Vararg{Symbol}}
-    vars = extract_variable_names(expr, loop_vars)
+    vars = extract_variable_names_and_numdims(expr, loop_vars)
     if vars === (;)
         return variables
     end
@@ -209,19 +198,18 @@ Extract all the variables assigned to in the program.
 
 # Example:
 ```jldoctest
-julia> JuliaBUGS.extract_variables_assigned_to(model_def)
+julia> JuliaBUGS.extract_variables_assigned_to(BUGSExamples.leuk.model_def)
 ((:c, :r), (:beta,), (Symbol("dL0.star"), :dN, :mu, Symbol("S.treat"), Symbol("S.placebo"), :Y, :Idt), (:dN, :dL0))
 ```
 """
 function extract_variables_assigned_to(expr::Expr)
     return Tuple.(
-        extract_variables_assigned_to_block(
+        extract_variables_assigned_to(
             expr, Symbol[], Symbol[], Set{Symbol}(), Set{Symbol}()
         )
     )
 end
-
-function extract_variables_assigned_to_block(
+function extract_variables_assigned_to(
     expr::Expr,
     logical_scalars::Vector{Symbol},
     stochastic_scalars::Vector{Symbol},
@@ -253,10 +241,40 @@ function extract_variables_assigned_to_block(
                 body_
             end
         )
-            extract_variables_assigned_to_block(
+            extract_variables_assigned_to(
                 body, logical_scalars, stochastic_scalars, logical_arrays, stochastic_arrays
             )
         end
     end
     return logical_scalars, stochastic_scalars, logical_arrays, stochastic_arrays
+end
+
+"""
+    extract_statement_loop_bounds(model_def::Expr)
+
+Extract the loop bounds for each statement in the program. If a variable is not in a loop, return an empty tuple.
+"""
+function extract_statement_loop_bounds(model_def::Expr)
+    return extract_statement_loop_bounds!(model_def, (), Tuple{Vararg{Expr}}[])
+end
+function extract_statement_loop_bounds!(
+    model_def::Expr,
+    loop_bounds::Tuple{Vararg{Expr}},
+    stmt_loop_bounds::Vector{Tuple{Vararg{Expr}}},
+)
+    for statement in model_def.args
+        if @capture(statement, (lhs_ = rhs_) | (lhs_ ~ rhs_))
+            push!(stmt_loop_bounds, loop_bounds)
+        elseif @capture(
+            statement,
+            for loop_var_ in loop_bound_
+                body_
+            end
+        )
+            extract_statement_loop_bounds!(
+                body, (loop_bounds..., loop_bound), stmt_loop_bounds
+            )
+        end
+    end
+    return stmt_loop_bounds
 end
