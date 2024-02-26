a069b4994fd7d5f8fe6f647ab8fb7a2550d94e56
Xianda Sun <sunxdt@gmail.com>
2024-02-26 18:44:04 +0000

    add a test for `decompose_for_expr`

--- test/utils.jl
+++ test/utils.jl
@@ -1,3 +1,24 @@
+@testset "Decompose for loop" begin
+    ex = MacroTools.@q for i in 1:3
+        x[i] = i
+        for j in 1:3
+            y[i, j] = i + j
+        end
+    end
+
+    loop_var, lb, ub, body = JuliaBUGS.decompose_for_expr(ex)
+    
+    @test loop_var == :i
+    @test lb == 1
+    @test ub == 3
+    @test body == MacroTools.@q begin
+        x[i] = i
+        for j in 1:3
+            y[i, j] = i + j
+        end
+    end
+end
+
 # Tests for `getparams`, using `Rats` model
 @testset "`getparams` with Rats" begin
     m = :rats
