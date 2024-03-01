a8398fcd40c332499e1674e6b4f7db6e324c73cc
Xianda Sun <sunxdt@gmail.com>
2024-03-01 08:22:29 +0000

    comment out other test for faster results

--- test/runtests.jl
+++ test/runtests.jl
@@ -46,93 +46,93 @@ using UnPack
 
 AbstractMCMC.setprogress!(false)
 
-@testset "Function Unit Tests" begin
-    DocMeta.setdocmeta!(
-        JuliaBUGS,
-        :DocTestSetup,
-        :(using JuliaBUGS:
-            JuliaBUGS,
-            BUGSExamples,
-            @bugs,
-            Var,
-            create_array_var,
-            replace_constants_in_expr,
-            evaluate_and_track_dependencies,
-            find_variables_on_lhs,
-            evaluate,
-            merge_with_coalescence,
-            scalarize,
-            concretize_colon_indexing,
-            extract_variable_names_and_numdims,
-            extract_variables_in_bounds_and_lhs_indices,
-            simple_arithmetic_eval);
-        recursive=true,
-    )
-    Documenter.doctest(JuliaBUGS; manual=false)
-end
+# @testset "Function Unit Tests" begin
+#     DocMeta.setdocmeta!(
+#         JuliaBUGS,
+#         :DocTestSetup,
+#         :(using JuliaBUGS:
+#             JuliaBUGS,
+#             BUGSExamples,
+#             @bugs,
+#             Var,
+#             create_array_var,
+#             replace_constants_in_expr,
+#             evaluate_and_track_dependencies,
+#             find_variables_on_lhs,
+#             evaluate,
+#             merge_with_coalescence,
+#             scalarize,
+#             concretize_colon_indexing,
+#             extract_variable_names_and_numdims,
+#             extract_variables_in_bounds_and_lhs_indices,
+#             simple_arithmetic_eval);
+#         recursive=true,
+#     )
+#     Documenter.doctest(JuliaBUGS; manual=false)
+# end
 
-include("bugs_primitives.jl")
+# include("bugs_primitives.jl")
 
-@testset "Parser" begin
-    include("parser/bugs_macro.jl")
-    include("parser/bugs_parser.jl")
-    include("parser/winbugs_examples.jl")
-end
+# @testset "Parser" begin
+#     include("parser/bugs_macro.jl")
+#     include("parser/bugs_parser.jl")
+#     include("parser/winbugs_examples.jl")
+# end
 
-include("compile.jl")
+# include("compile.jl")
 
-@testset "Compile WinBUGS Vol I examples: $m" for m in [
-    :blockers,
-    :bones,
-    :dogs,
-    :dyes,
-    :epil,
-    :equiv,
-    :kidney,
-    :leuk,
-    :leukfr,
-    :lsat,
-    :magnesium,
-    :mice,
-    :oxford,
-    :pumps,
-    :rats,
-    :salm,
-    :seeds,
-    :stacks,
-    :surgical_simple,
-    :surgical_realistic,
-]
-    model_def = JuliaBUGS.BUGSExamples.VOLUME_I[m].model_def
-    data = JuliaBUGS.BUGSExamples.VOLUME_I[m].data
-    inits = JuliaBUGS.BUGSExamples.VOLUME_I[m].inits[1]
-    model = compile(model_def, data, inits)
-end
+# @testset "Compile WinBUGS Vol I examples: $m" for m in [
+#     :blockers,
+#     :bones,
+#     :dogs,
+#     :dyes,
+#     :epil,
+#     :equiv,
+#     :kidney,
+#     :leuk,
+#     :leukfr,
+#     :lsat,
+#     :magnesium,
+#     :mice,
+#     :oxford,
+#     :pumps,
+#     :rats,
+#     :salm,
+#     :seeds,
+#     :stacks,
+#     :surgical_simple,
+#     :surgical_realistic,
+# ]
+#     model_def = JuliaBUGS.BUGSExamples.VOLUME_I[m].model_def
+#     data = JuliaBUGS.BUGSExamples.VOLUME_I[m].data
+#     inits = JuliaBUGS.BUGSExamples.VOLUME_I[m].inits[1]
+#     model = compile(model_def, data, inits)
+# end
 
-@testset "Utils" begin
-    include("utils.jl")
-end
+# @testset "Utils" begin
+#     include("utils.jl")
+# end
 
-@testset "Log Probability Test" begin
-    include("run_logp_tests.jl")
-    @testset "Single stochastic variable test" begin
-        @testset "test for $s" for s in [:binomial, :gamma, :lkj, :dwish, :ddirich]
-            include("logp_tests/$s.jl")
-        end
-    end
-    @testset "BUGS examples" begin
-        @testset "test for $s" for s in [:blockers, :bones, :dogs, :rats]
-            include("logp_tests/$s.jl")
-        end
-    end
-end
+# @testset "Log Probability Test" begin
+#     include("run_logp_tests.jl")
+#     @testset "Single stochastic variable test" begin
+#         @testset "test for $s" for s in [:binomial, :gamma, :lkj, :dwish, :ddirich]
+#             include("logp_tests/$s.jl")
+#         end
+#     end
+#     @testset "BUGS examples" begin
+#         @testset "test for $s" for s in [:blockers, :bones, :dogs, :rats]
+#             include("logp_tests/$s.jl")
+#         end
+#     end
+# end
 
-@testset "Graph data structure" begin
-    include("graphs.jl")
-end
+# @testset "Graph data structure" begin
+#     include("graphs.jl")
+# end
 
-include("gibbs.jl")
+# include("gibbs.jl")
 
-include("ext/mcmchains.jl")
+# include("ext/mcmchains.jl")
 
 include("profile.jl")
