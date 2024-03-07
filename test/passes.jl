6ed4c364e7aff7161cb2d2b4fa4c272c1cbbdfc7
Xianda Sun <sunxdt@gmail.com>
2024-03-07 15:25:22 +0000

    format and enable test

--- test/passes.jl
+++ test/passes.jl
@@ -1,4 +1,4 @@
-using JuliaBUGS: analyze_program, CollectVariables
+using JuliaBUGS: analyze_program, CollectVariables, DataTransformation
 using JuliaBUGS: is_resolved
 using JuliaBUGS: is_specified_by_data
 
@@ -7,32 +7,40 @@ using JuliaBUGS: is_specified_by_data
     model_def = @bugs begin
         b = a
     end
-    data = (b = [1, 2],)
-    @test_throws ErrorException analyze_program(CollectVariables(model_def, data), model_def, data)
+    data = (b=[1, 2],)
+    @test_throws ErrorException analyze_program(
+        CollectVariables(model_def, data), model_def, data
+    )
 
     model_def = @bugs begin
         x[1:3] = y[1:3]
     end
-    data = (x = [1, missing, missing], y = [1, 2, 3])
-    @test_throws ErrorException analyze_program(CollectVariables(model_def, data), model_def, data)
+    data = (x=[1, missing, missing], y=[1, 2, 3])
+    @test_throws ErrorException analyze_program(
+        CollectVariables(model_def, data), model_def, data
+    )
 
     # partially specified as data
     model_def = @bugs begin
-        x[1:3] ~ dmnorm(y[1:3], E[:,:])
+        x[1:3] ~ dmnorm(y[1:3], E[:, :])
     end
-    data = (x = [1, missing, missing], y = [1, 2, 3], E = [1 0 0; 0 1 0; 0 0 1])
-    @test_throws ErrorException analyze_program(CollectVariables(model_def, data), model_def, data)
+    data = (x=[1, missing, missing], y=[1, 2, 3], E=[1 0 0; 0 1 0; 0 0 1])
+    @test_throws ErrorException analyze_program(
+        CollectVariables(model_def, data), model_def, data
+    )
 
     # check access data array out-of-bound
     model_def = @bugs begin
         x[4] = 2
     end
-    data = (x = [1, 2, 3],)
-    @test_throws BoundsError analyze_program(CollectVariables(model_def, data), model_def, data)
+    data = (x=[1, 2, 3],)
+    @test_throws BoundsError analyze_program(
+        CollectVariables(model_def, data), model_def, data
+    )
 end
 
 @testset "is_specified_by_data" begin
-    data = (a = [1, 2, 3], b = 2, c = [1, 2, missing], d = [missing, missing])
+    data = (a=[1, 2, 3], b=2, c=[1, 2, missing], d=[missing, missing])
     @test is_specified_by_data(data, :b)
     @test_throws ErrorException is_specified_by_data(data, :c)
     @test !is_specified_by_data(data, :d, 1)
@@ -51,9 +59,16 @@ end
     end
     data = (b=1, e=[1, 2])
 
-    scalars, array_sizes = analyze_program(CollectVariables(), model_def, data)
+    scalars, array_sizes = analyze_program(
+        CollectVariables(model_def, data), model_def, data
+    )
+
+    transformed_variables = Dict{Symbol,Any}(
+        :a => missing, :c => missing, :d => Union{Int,Missing}[missing,]
+    )
+
     has_new_val, transformed_variables = analyze_program(
-        DataTransformation(scalars, array_sizes), model_def, data
+        DataTransformation(false, transformed_variables), model_def, data
     )
     @test has_new_val == true
     @test transformed_variables[:a] == 2
@@ -68,37 +83,4 @@ end
         DataTransformation(false, transformed_variables), model_def, data
     )
     @test has_new_val == false
-end
-
-@testset "Test compiler passes with $m" for m in [:leuk, :leukfr]
-    model_def = JuliaBUGS.BUGSExamples.VOLUME_I[m].model_def
-    data = JuliaBUGS.BUGSExamples.VOLUME_I[m].data
-    inits = JuliaBUGS.BUGSExamples.VOLUME_I[m].inits[1]
-
-    scalars, array_sizes = analyze_program(CollectVariables(), model_def, data)
-
-    has_new_val, transformed_variables = analyze_program(
-        DataTransformation(scalars, array_sizes), model_def, data
-    )
-    @test has_new_val == true
-    @test all(!ismissing, transformed_variables[:Y])
-
-    has_new_val, transformed_variables = analyze_program(
-        DataTransformation(false, transformed_variables), model_def, data
-    )
-
-    @test has_new_val == true
-    @test all(!ismissing, transformed_variables[:dN])
-
-    array_bitmap, transformed_variables = analyze_program(
-        PostChecking(data, transformed_variables), model_def, data
-    )
-
-    vars, array_sizes, array_bitmap, node_args, node_functions, dependencies = analyze_program(
-        NodeFunctions(array_sizes, array_bitmap),
-        model_def,
-        merge_with_coalescence(data, transformed_variables),
-    )
-
-    compile(model_def, data, inits)
 end
