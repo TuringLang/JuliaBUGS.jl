6edb1537715c4e1f8e5ea68dde1115b0b240cf39
Xianda Sun <sunxdt@gmail.com>
2024-03-25 12:02:53 +0000

    fix file path

--- test/logp_tests/test_logp.jl
+++ test/logp_tests/test_logp.jl
@@ -5,12 +5,12 @@ using DynamicPPL: DynamicPPL, getlogp, settrans!!, SimpleVarInfo
 @testset "Log joint probability" begin
     @testset "Single distribution models" begin
         @testset "$s" for s in [:binomial, :gamma, :lkj, :dwish, :ddirich]
-            include(joinpath(dirname(@__FILE__), "/single_distribution_models/$s.jl"))
+            include("single_distribution_models/$s.jl")
         end
     end
     @testset "BUGS models" begin
         @testset "$s" for s in [:blockers, :bones, :dogs, :rats]
-            include(joinpath(dirname(@__FILE__), "/BUGS_models/$s.jl"))
+            include("BUGS_models/$s.jl")
         end
     end
 end
