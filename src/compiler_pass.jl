43c705d17fbfd968fb1fea2b98713f88ed7bc777
Xianda Sun <sunxdt@gmail.com>
2024-04-25 18:43:04 +0100

    use `identity` instead of `IdentityLens`

--- src/compiler_pass.jl
+++ src/compiler_pass.jl
@@ -704,10 +704,10 @@ function analyze_statement(pass::AddVertices, expr::Expr, loop_vars::NamedTuple)
     args, node_function_expr, node_function = pass.f_dict[expr]
 
     vn = if lhs isa Symbol
-        AbstractPPL.VarName{lhs}(AbstractPPL.IdentityLens())
+        AbstractPPL.VarName{lhs}(identity)
     else
         v, indices... = lhs
-        AbstractPPL.VarName{v}(AbstractPPL.IndexLens(indices))
+        AbstractPPL.VarName{v}(identity)
     end
     add_vertex!(
         pass.g,
