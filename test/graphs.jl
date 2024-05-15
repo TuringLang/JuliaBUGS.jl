61d2e9b354e77cc35aa1e8fbcfaca78bb67dbf8f
Xianda Sun <5433119+sunxd3@users.noreply.github.com>
2024-05-15 15:54:49 +0100

    Update test/graphs.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/graphs.jl
+++ test/graphs.jl
@@ -16,11 +16,11 @@ end
 
 # construct a SimpleVarInfo
 inits = (
-    a = 1.0,
-    b = 2.0,
-    c = 3.0,
-    d = 4.0,
-    e = 5.0,
+    a=1.0,
+    b=2.0,
+    c=3.0,
+    d=4.0,
+    e=5.0,
 
     # f = 1.0,
     # g = 2.0,
