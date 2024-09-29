77eadf28f9cbe248c7d1b0d93ce5663322d2823b
Xianda Sun <sunxdt@gmail.com>
2024-09-29 10:37:23 +0100

    reorder pages

--- docs/make.jl
+++ docs/make.jl
@@ -9,14 +9,14 @@ makedocs(;
     pages=[
         "Home" => "index.md",
         "Example" => "example.md",
-        "Differences from Other BUGS Implementations" => "differences.md",
-        "Pitfalls" => "pitfalls.md",
         "API" => [
             "General" => "api/api.md",
             "Functions" => "api/functions.md",
             "Distributions" => "api/distributions.md",
             "User-Defined Functions and Distributions" => "api/user_defined_functions.md",
         ],
+        "Differences from Other BUGS Implementations" => "differences.md",
+        "Pitfalls" => "pitfalls.md",
         "Plotting" => "graph_plotting.md",
         "R Interface" => "R_interface.md",
         "For Developers" =>
