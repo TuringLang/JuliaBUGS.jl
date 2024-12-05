164a6cefef0a6c598cc250a5fd443e7986d8e5e0
Xianda Sun <sunxdt@gmail.com>
2024-12-05 09:17:45 +0000

    add implementation

--- src/model.jl
+++ src/model.jl
@@ -539,3 +539,67 @@ function AbstractPPL.evaluate!!(model::BUGSModel, flattened_values::AbstractVect
     end
     return evaluation_env, logp
 end
+
+"""
+    _tempered_evaluate!!(model::BUGSModel, flattened_values::AbstractVector; temperature=1.0)
+
+Evaluating the model with the given model parameter values, returns updated evaluation environment 
+and a NamedTuple of logprior, loglikelihood and tempered logjoint (where tempered logjoint is the logjoint 
+whose loglikelihood component scaled by the given temperature).
+"""
+function _tempered_evaluate!!(
+    model::BUGSModel, flattened_values::AbstractVector; temperature=1.0
+)
+    var_lengths = if model.transformed
+        model.transformed_var_lengths
+    else
+        model.untransformed_var_lengths
+    end
+
+    evaluation_env = deepcopy(model.evaluation_env)
+    current_idx = 1
+    logprior, loglikelihood = 0.0, 0.0
+    for (i, vn) in enumerate(model.flattened_graph_node_data.sorted_nodes)
+        is_stochastic = model.flattened_graph_node_data.is_stochastic_vals[i]
+        is_observed = model.flattened_graph_node_data.is_observed_vals[i]
+        node_function = model.flattened_graph_node_data.node_function_vals[i]
+        loop_vars = model.flattened_graph_node_data.loop_vars_vals[i]
+        if !is_stochastic
+            value = node_function(evaluation_env, loop_vars)
+            evaluation_env = BangBang.setindex!!(evaluation_env, value, vn)
+        else
+            dist = node_function(evaluation_env, loop_vars)
+            if !is_observed
+                l = var_lengths[vn]
+                if model.transformed
+                    b = Bijectors.bijector(dist)
+                    b_inv = Bijectors.inverse(b)
+                    reconstructed_value = reconstruct(
+                        b_inv,
+                        dist,
+                        view(flattened_values, current_idx:(current_idx + l - 1)),
+                    )
+                    value, logjac = Bijectors.with_logabsdet_jacobian(
+                        b_inv, reconstructed_value
+                    )
+                else
+                    value = reconstruct(
+                        dist, view(flattened_values, current_idx:(current_idx + l - 1))
+                    )
+                    logjac = 0.0
+                end
+                current_idx += l
+                logprior += logpdf(dist, value) + logjac
+                evaluation_env = BangBang.setindex!!(evaluation_env, value, vn)
+            else
+                loglikelihood += logpdf(dist, AbstractPPL.get(evaluation_env, vn))
+            end
+        end
+    end
+    return evaluation_env,
+    (
+        logprior=logprior,
+        loglikelihood=loglikelihood,
+        tempered_logjoint=logprior + temperature * loglikelihood,
+    )
+end
