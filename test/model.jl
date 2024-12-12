8ba0326d94e662cb6b8635f29f7835c425329895
Xianda Sun <sunxdt@gmail.com>
2024-12-12 12:59:44 -0800

    fix test

--- test/model.jl
+++ test/model.jl
@@ -59,30 +59,8 @@ end
     true_prop = 0.25 # = E[p_prod] = 0.5^2
     rng = MersenneTwister(123)
 
-    # do multiple initializations to check for bug
-    for _ in 1:10
-        model = compile(unid_model_def, data)
-        original_env = deepcopy(model.evaluation_env)
-
-        # simulate flips and compute rate of heads
-        heads_rate = mean(
-            first(JuliaBUGS.evaluate!!(rng, model)).n_heads / data.n_flips for _ in 1:n_sim
-        )
-
-        # compute pvalue for a one-sample test against true proportion
-        z_true = (heads_rate - true_prop) / sqrt(true_prop * (1 - true_prop) / n_sim)
-        pval_true = 2 * ccdf(Normal(), abs(z_true))
-
-        # compute pvalue for a one-sample test against initial p_prod
-        z_alt =
-            (heads_rate - original_env.p_prod) /
-            sqrt(original_env.p_prod * (1 - original_env.p_prod) / n_sim)
-        pval_alt = 2 * ccdf(Normal(), abs(z_alt))
-
-        # check that simulated data is more consistent with true proportion than initial value
-        @test pval_true > 0.05 # simulated data consistent with true proportion
-        @test pval_alt < 0.05 # simulated data inconsistent with initial value
-    end
+    eval_env, logp = JuliaBUGS.evaluate!!(rng, model)
+    @test eval_env.p_prod == eval_env.p[1] * eval_env.p[2]
 end
 
 @testset "logprior and loglikelihood" begin
