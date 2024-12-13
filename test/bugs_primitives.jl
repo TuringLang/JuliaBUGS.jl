a4cd10dd2429e2aad33dd69a4309988fed772196
Seren Lee <seren.lee1127@gmail.com>
2024-12-13 10:01:35 -0800

    Update test bugs_primitives.jl

--- test/bugs_primitives.jl
+++ test/bugs_primitives.jl
@@ -27,3 +27,9 @@ end
     ]
     LogDensityProblems.logdensity_and_gradient(ad_model, theta)
 end
+
+@testset "inprod(a,b)" begin
+    A = [3, 0.7, 6]
+    B = [2.6, 1.3, 3]
+    @test  JuliaBUGS.BUGSPrimitives.inprod(A,B) = 26.71
+end
