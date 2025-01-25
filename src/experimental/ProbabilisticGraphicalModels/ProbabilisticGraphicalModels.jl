b1d64d64db2a0ebe3a501dadda599705277c0adc
Ryan Xiao Wei Lin <rxwl2@cam.ac.uk>
2025-01-25 16:49:40 +0000

    changes

--- src/experimental/ProbabilisticGraphicalModels/ProbabilisticGraphicalModels.jl
+++ src/experimental/ProbabilisticGraphicalModels/ProbabilisticGraphicalModels.jl
@@ -8,6 +8,12 @@ include("bayesian_network.jl")
 include("conditioning.jl")
 include("functions.jl")
 
-export BayesianNetwork, condition, condition!, decondition, decondition!, ancestral_sampling, is_conditionally_independent
+export BayesianNetwork,
+    condition,
+    condition!,
+    decondition,
+    decondition!,
+    ancestral_sampling,
+    is_conditionally_independent
 
 end
