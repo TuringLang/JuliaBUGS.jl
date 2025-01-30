420670992d6a21a205c489109b46fd0736109af3
Ryan Xiao Wei Lin <rxwl2@cam.ac.uk>
2025-01-30 16:25:15 +0000

    Revert "revert"
    This reverts commit 4edbdebfa0fa9284f573a2440d6a9e983a3767b8.

--- test/parser/winbugs_examples.jl
+++ test/parser/winbugs_examples.jl
@@ -24,106 +24,102 @@
 """)
 
     # oxford
-    parse_bugs(
-        """
-model
-   {
-      for (i in 1 : K) {
-         r0[i] ~ dbin(p0[i], n0[i])
-         r1[i] ~ dbin(p1[i], n1[i])
-         logit(p0[i]) <- mu[i]
-         logit(p1[i]) <- mu[i] + logPsi[i]
-         logPsi[i] <- alpha + beta1 * year[i] + beta2 * (year[i] * year[i] - 22) + b[i]
-         b[i] ~ dnorm(0, tau)
-         mu[i] ~ dnorm(0.0, 1.0E-6)
-         cumulative.r0[i] <- cumulative(r0[i], r0[i])
-         cumulative.r1[i] <- cumulative(r1[i], r1[i])
-      }
-      alpha ~ dnorm(0.0, 1.0E-6)
-      beta1 ~ dnorm(0.0, 1.0E-6)
-      beta2 ~ dnorm(0.0, 1.0E-6)
-      tau ~ dgamma(1.0E-3, 1.0E-3)
-      sigma <- 1 / sqrt(tau)
-   }
-"""
-    )
+    parse_bugs("""
+    model
+       {
+          for (i in 1 : K) {
+             r0[i] ~ dbin(p0[i], n0[i])
+             r1[i] ~ dbin(p1[i], n1[i])
+             logit(p0[i]) <- mu[i]
+             logit(p1[i]) <- mu[i] + logPsi[i]
+             logPsi[i] <- alpha + beta1 * year[i] + beta2 * (year[i] * year[i] - 22) + b[i]
+             b[i] ~ dnorm(0, tau)
+             mu[i] ~ dnorm(0.0, 1.0E-6)
+             cumulative.r0[i] <- cumulative(r0[i], r0[i])
+             cumulative.r1[i] <- cumulative(r1[i], r1[i])
+          }
+          alpha ~ dnorm(0.0, 1.0E-6)
+          beta1 ~ dnorm(0.0, 1.0E-6)
+          beta2 ~ dnorm(0.0, 1.0E-6)
+          tau ~ dgamma(1.0E-3, 1.0E-3)
+          sigma <- 1 / sqrt(tau)
+       }
+    """)
 
     # inhalers
-    parse_bugs(
-        """
-model
-{
-#
-# Construct individual response data from contingency table
-#
-   for (i in 1 : Ncum[1, 1]) {
-      group[i] <- 1
-      for (t in 1 : T) { response[i, t] <- pattern[1, t] }
-   }
-   for (i in (Ncum[1,1] + 1) : Ncum[1, 2]) {
-      group[i] <- 2 for (t in 1 : T) { response[i, t] <- pattern[1, t] }
-   }
+    parse_bugs("""
+  model
+  {
+  #
+  # Construct individual response data from contingency table
+  #
+     for (i in 1 : Ncum[1, 1]) {
+        group[i] <- 1
+        for (t in 1 : T) { response[i, t] <- pattern[1, t] }
+     }
+     for (i in (Ncum[1,1] + 1) : Ncum[1, 2]) {
+        group[i] <- 2 for (t in 1 : T) { response[i, t] <- pattern[1, t] }
+     }
 
-   for (k in 2 : Npattern) {
-      for(i in (Ncum[k - 1, 2] + 1) : Ncum[k, 1]) {
-         group[i] <- 1 for (t in 1 : T) { response[i, t] <- pattern[k, t] }
-      }
-      for(i in (Ncum[k, 1] + 1) : Ncum[k, 2]) {
-         group[i] <- 2 for (t in 1 : T) { response[i, t] <- pattern[k, t] }
-      }
-   }
-#
-# Model
-#
-   for (i in 1 : N) {
-      for (t in 1 : T) {
-         for (j in 1 : Ncut) {
-#
-# Cumulative probability of worse response than j
-#
-            logit(Q[i, t, j]) <- -(a[j] + mu[group[i], t] + b[i])
-         }
-#
-# Probability of response = j
-#
-         p[i, t, 1] <- 1 - Q[i, t, 1]
-         for (j in 2 : Ncut) { p[i, t, j] <- Q[i, t, j - 1] - Q[i, t, j] }
-         p[i, t, (Ncut+1)] <- Q[i, t, Ncut]
+     for (k in 2 : Npattern) {
+        for(i in (Ncum[k - 1, 2] + 1) : Ncum[k, 1]) {
+           group[i] <- 1 for (t in 1 : T) { response[i, t] <- pattern[k, t] }
+        }
+        for(i in (Ncum[k, 1] + 1) : Ncum[k, 2]) {
+           group[i] <- 2 for (t in 1 : T) { response[i, t] <- pattern[k, t] }
+        }
+     }
+  #
+  # Model
+  #
+     for (i in 1 : N) {
+        for (t in 1 : T) {
+           for (j in 1 : Ncut) {
+  #
+  # Cumulative probability of worse response than j
+  #
+              logit(Q[i, t, j]) <- -(a[j] + mu[group[i], t] + b[i])
+           }
+  #
+  # Probability of response = j
+  #
+           p[i, t, 1] <- 1 - Q[i, t, 1]
+           for (j in 2 : Ncut) { p[i, t, j] <- Q[i, t, j - 1] - Q[i, t, j] }
+           p[i, t, (Ncut+1)] <- Q[i, t, Ncut]
 
-         response[i, t] ~ dcat(p[i, t, ])
-         cumulative.response[i, t] <- cumulative(response[i, t], response[i, t])
-      }
-#
-# Subject (random) effects
-#
-      b[i] ~ dnorm(0.0, tau)
-}
+           response[i, t] ~ dcat(p[i, t, ])
+           cumulative.response[i, t] <- cumulative(response[i, t], response[i, t])
+        }
+  #
+  # Subject (random) effects
+  #
+        b[i] ~ dnorm(0.0, tau)
+  }
 
-#
-# Fixed effects
-#
-   for (g in 1 : G) {
-      for(t in 1 : T) {
-# logistic mean for group i in period t
-         mu[g, t] <- beta * treat[g, t] / 2 + pi * period[g, t] / 2 + kappa * carry[g, t]
-      }
-   }
-   beta ~ dnorm(0, 1.0E-06)
-   pi ~ dnorm(0, 1.0E-06)
-   kappa ~ dnorm(0, 1.0E-06)
+  #
+  # Fixed effects
+  #
+     for (g in 1 : G) {
+        for(t in 1 : T) {
+  # logistic mean for group i in period t
+           mu[g, t] <- beta * treat[g, t] / 2 + pi * period[g, t] / 2 + kappa * carry[g, t]
+        }
+     }
+     beta ~ dnorm(0, 1.0E-06)
+     pi ~ dnorm(0, 1.0E-06)
+     kappa ~ dnorm(0, 1.0E-06)
 
-# ordered cut points for underlying continuous latent variable
-   a[1] ~ dflat()T(-1000, a[2])
-   a[2] ~ dflat()T(a[1], a[3])
-   a[3] ~ dflat()T(a[2], 1000)
+  # ordered cut points for underlying continuous latent variable
+     a[1] ~ dflat()T(-1000, a[2])
+     a[2] ~ dflat()T(a[1], a[3])
+     a[3] ~ dflat()T(a[2], 1000)
 
-   tau ~ dgamma(0.001, 0.001)
-   sigma <- sqrt(1 / tau)
-   log.sigma <- log(sigma)
+     tau ~ dgamma(0.001, 0.001)
+     sigma <- sqrt(1 / tau)
+     log.sigma <- log(sigma)
 
-}
-"""
-    )
+  }
+  """)
 
     # pumps
     parse_bugs("""
@@ -512,80 +508,78 @@ model
     """)
 
     # Inhaler
-    parse_bugs(
-        """
-model
-{
-#
-# Construct individual response data from contingency table
-#
-   for (i in 1 : Ncum[1, 1]) {
-      group[i] <- 1
-      for (t in 1 : T) { response[i, t] <- pattern[1, t] }
-   }
-   for (i in (Ncum[1,1] + 1) : Ncum[1, 2]) {
-      group[i] <- 2 for (t in 1 : T) { response[i, t] <- pattern[1, t] }
-   }
+    parse_bugs("""
+  model
+  {
+  #
+  # Construct individual response data from contingency table
+  #
+     for (i in 1 : Ncum[1, 1]) {
+        group[i] <- 1
+        for (t in 1 : T) { response[i, t] <- pattern[1, t] }
+     }
+     for (i in (Ncum[1,1] + 1) : Ncum[1, 2]) {
+        group[i] <- 2 for (t in 1 : T) { response[i, t] <- pattern[1, t] }
+     }
 
-   for (k in 2 : Npattern) {
-      for(i in (Ncum[k - 1, 2] + 1) : Ncum[k, 1]) {
-         group[i] <- 1 for (t in 1 : T) { response[i, t] <- pattern[k, t] }
-      }
-      for(i in (Ncum[k, 1] + 1) : Ncum[k, 2]) {
-         group[i] <- 2 for (t in 1 : T) { response[i, t] <- pattern[k, t] }
-      }
-   }
-#
-# Model
-#
-   for (i in 1 : N) {
-      for (t in 1 : T) {
-         for (j in 1 : Ncut) {
-#
-# Cumulative probability of worse response than j
-#
-            logit(Q[i, t, j]) <- -(a[j] + mu[group[i], t] + b[i])
-         }
-#
-# Probability of response = j
-#
-         p[i, t, 1] <- 1 - Q[i, t, 1]
-         for (j in 2 : Ncut) { p[i, t, j] <- Q[i, t, j - 1] - Q[i, t, j] }
-         p[i, t, (Ncut+1)] <- Q[i, t, Ncut]
+     for (k in 2 : Npattern) {
+        for(i in (Ncum[k - 1, 2] + 1) : Ncum[k, 1]) {
+           group[i] <- 1 for (t in 1 : T) { response[i, t] <- pattern[k, t] }
+        }
+        for(i in (Ncum[k, 1] + 1) : Ncum[k, 2]) {
+           group[i] <- 2 for (t in 1 : T) { response[i, t] <- pattern[k, t] }
+        }
+     }
+  #
+  # Model
+  #
+     for (i in 1 : N) {
+        for (t in 1 : T) {
+           for (j in 1 : Ncut) {
+  #
+  # Cumulative probability of worse response than j
+  #
+              logit(Q[i, t, j]) <- -(a[j] + mu[group[i], t] + b[i])
+           }
+  #
+  # Probability of response = j
+  #
+           p[i, t, 1] <- 1 - Q[i, t, 1]
+           for (j in 2 : Ncut) { p[i, t, j] <- Q[i, t, j - 1] - Q[i, t, j] }
+           p[i, t, (Ncut+1)] <- Q[i, t, Ncut]
 
-         response[i, t] ~ dcat(p[i, t, ])
-      }
-#
-# Subject (random) effects
-#
-      b[i] ~ dnorm(0.0, tau)
-}
+           response[i, t] ~ dcat(p[i, t, ])
+        }
+  #
+  # Subject (random) effects
+  #
+        b[i] ~ dnorm(0.0, tau)
+  }
 
-#
-# Fixed effects
-#
-   for (g in 1 : G) {
-      for(t in 1 : T) {
-# logistic mean for group i in period t
-         mu[g, t] <- beta * treat[g, t] / 2 + pi * period[g, t] / 2 + kappa * carry[g, t]
-      }
-   }
-   beta ~ dnorm(0, 1.0E-06)
-   pi ~ dnorm(0, 1.0E-06)
-   kappa ~ dnorm(0, 1.0E-06)
+  #
+  # Fixed effects
+  #
+     for (g in 1 : G) {
+        for(t in 1 : T) {
+  # logistic mean for group i in period t
+           mu[g, t] <- beta * treat[g, t] / 2 + pi * period[g, t] / 2 + kappa * carry[g, t]
+        }
+     }
+     beta ~ dnorm(0, 1.0E-06)
+     pi ~ dnorm(0, 1.0E-06)
+     kappa ~ dnorm(0, 1.0E-06)
 
-# ordered cut points for underlying continuous latent variable
-   a[1] ~ dunif(-1000, a[2])
-   a[2] ~ dunif(a[1], a[3])
-   a[3] ~ dunif(a[2], 1000)
+  # ordered cut points for underlying continuous latent variable
+     a[1] ~ dunif(-1000, a[2])
+     a[2] ~ dunif(a[1], a[3])
+     a[3] ~ dunif(a[2], 1000)
 
-   tau ~ dgamma(0.001, 0.001)
-   sigma <- sqrt(1 / tau)
-   log.sigma <- log(sigma)
+     tau ~ dgamma(0.001, 0.001)
+     sigma <- sqrt(1 / tau)
+     log.sigma <- log(sigma)
 
-}
-"""
-    )
+  }
+  """)
 
     # Mice
     parse_bugs("""
@@ -947,45 +941,43 @@ model
     """)
 
     # Ice
-    parse_bugs(
-        """
-model
-{
-   for (i in 1:I) {
-      cases[i] ~ dpois(mu[i])
-      log(mu[i]) <- log(pyr[i]) + alpha[age[i]] + beta[year[i]]
-   }
-   betamean[1] <- 2 * beta[2] - beta[3]
-   Nneighs[1] <- 1
-   betamean[2] <- (2 * beta[1] + 4 * beta[3] - beta[4]) / 5
-   Nneighs[2] <- 5
-   for (k in 3 : K - 2) {
-      betamean[k] <- (4 * beta[k - 1] + 4 * beta[k + 1]- beta[k - 2] - beta[k + 2]) / 6
-      Nneighs[k] <- 6
-   }
-   betamean[K - 1] <- (2 * beta[K] + 4 * beta[K - 2] - beta[K - 3]) / 5
-   Nneighs[K - 1] <- 5
-   betamean[K] <- 2 * beta[K - 1] - beta[K - 2]
-   Nneighs[K] <- 1
-   for (k in 1 : K) {
-      betaprec[k] <- Nneighs[k] * tau
-   }
-   for (k in 1 : K) {
-      beta[k] ~ dnorm(betamean[k], betaprec[k])
-      logRR[k] <- beta[k] - beta[5]
-      tau.like[k] <- Nneighs[k] * beta[k] * (beta[k] - betamean[k])
-   }
-   alpha[1] <- 0.0
-   for (j in 2 : Nage) {
-      alpha[j] ~ dnorm(0, 1.0E-6)
-   }
-   d <- 0.0001 + sum(tau.like[]) / 2
-   r <- 0.0001 + K / 2
-   tau ~ dgamma(r, d)
-   sigma <- 1 / sqrt(tau)
-}
-"""
-    )
+    parse_bugs("""
+    model
+    {
+       for (i in 1:I) {
+          cases[i] ~ dpois(mu[i])
+          log(mu[i]) <- log(pyr[i]) + alpha[age[i]] + beta[year[i]]
+       }
+       betamean[1] <- 2 * beta[2] - beta[3]
+       Nneighs[1] <- 1
+       betamean[2] <- (2 * beta[1] + 4 * beta[3] - beta[4]) / 5
+       Nneighs[2] <- 5
+       for (k in 3 : K - 2) {
+          betamean[k] <- (4 * beta[k - 1] + 4 * beta[k + 1]- beta[k - 2] - beta[k + 2]) / 6
+          Nneighs[k] <- 6
+       }
+       betamean[K - 1] <- (2 * beta[K] + 4 * beta[K - 2] - beta[K - 3]) / 5
+       Nneighs[K - 1] <- 5
+       betamean[K] <- 2 * beta[K - 1] - beta[K - 2]
+       Nneighs[K] <- 1
+       for (k in 1 : K) {
+          betaprec[k] <- Nneighs[k] * tau
+       }
+       for (k in 1 : K) {
+          beta[k] ~ dnorm(betamean[k], betaprec[k])
+          logRR[k] <- beta[k] - beta[5]
+          tau.like[k] <- Nneighs[k] * beta[k] * (beta[k] - betamean[k])
+       }
+       alpha[1] <- 0.0
+       for (j in 2 : Nage) {
+          alpha[j] ~ dnorm(0, 1.0E-6)
+       }
+       d <- 0.0001 + sum(tau.like[]) / 2
+       r <- 0.0001 + K / 2
+       tau ~ dgamma(r, d)
+       sigma <- 1 / sqrt(tau)
+    }
+    """)
 
     # Beetles
     parse_bugs("""
@@ -2107,7 +2099,7 @@ end
     tau.x ~ dgamma(a, b)
     tau.y ~ dgamma(a, b)
     }
-""",
+"""
     )
 
     # Five compartments
