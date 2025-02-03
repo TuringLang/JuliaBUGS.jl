9780e3fbf4aa60a27e1c09f59e4619561e2bd875
Xianda Sun <sunxdt@gmail.com>
2025-02-03 15:01:23 +0000

    minor updates

--- src/experimental/ProbabilisticGraphicalModels/ProbabilisticGraphicalModels.jl
+++ src/experimental/ProbabilisticGraphicalModels/ProbabilisticGraphicalModels.jl
@@ -2,34 +2,24 @@ module ProbabilisticGraphicalModels
 
 using BangBang
 using Graphs
+using MetaGraphsNext
 using Distributions
 using JuliaBUGS
-using JuliaBUGS: BUGSGraph, VarName
-using JuliaBUGS.MetaGraphsNext
-
-import Graphs: edges
-import JuliaBUGS.MetaGraphsNext: labels
+using JuliaBUGS: BUGSGraph, VarName, NodeInfo
 
 include("bayesian_network.jl")
 include("conditioning.jl")
 include("functions.jl")
 
-struct NodeInfo{F}
-    is_stochastic::Bool
-    is_observed::Bool
-    node_function_expr::Expr
-    node_function::F
-    node_args::Tuple{Vararg{Symbol}}
-    loop_vars::NamedTuple
-end
-
-export BayesianNetwork, condition, condition!, s
-decondition,
-decondition!,
-ancestral_sampling,
-is_conditionally_independent,
-add_deterministic_vertex!,
-add_stochastic_vertex!,
-add_vertex!,
-translate_BUGSGraph_to_BayesianNetwork
+export BayesianNetwork,
+    condition,
+    condition!,
+    decondition,
+    decondition!,
+    ancestral_sampling,
+    is_conditionally_independent,
+    add_deterministic_vertex!,
+    add_stochastic_vertex!,
+    add_vertex!,
+    translate_BUGSGraph_to_BayesianNetwork
 end
