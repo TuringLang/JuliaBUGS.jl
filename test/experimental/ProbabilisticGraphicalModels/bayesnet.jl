46453570ebad0a704f25fcd0c741898952aedcbc
Ryan Xiao Wei Lin <rxwl2@cam.ac.uk>
2025-02-24 19:36:19 +0000

    using symbol for conditioning

--- test/experimental/ProbabilisticGraphicalModels/bayesnet.jl
+++ test/experimental/ProbabilisticGraphicalModels/bayesnet.jl
@@ -436,22 +436,22 @@ using Bijectors: Bijectors
     #             x[i] ~ dnorm(i, 1)
     #         end
     #     end
-    
+
     #     loop_inits = NamedTuple{(:x,)}(( [1.0, 2.0, 3.0], ))
-    
+
     #     loop_compiled_model = compile(loop_model, NamedTuple(), loop_inits)
-    
+
     #     loop_g = loop_compiled_model.g
-    
+
     #     # Translate the loop-based BUGSGraph to a BayesianNetwork
     #     loop_bn = translate_BUGSGraph_to_BayesianNetwork(
     #         loop_g, loop_compiled_model.evaluation_env
     #     )
-    
+
     #     loop_evaluation_env, loop_logp = evaluate(loop_bn)
-    
+
     #     @test haskey(loop_evaluation_env, :x) && length(loop_evaluation_env[:x]) == 3
     #     @test loop_logp ≈ sum(logpdf(Normal(i, 1), loop_evaluation_env[:x][i]) for i in 1:3)
     # end
-    
+
 end
