90ca1a4a7043b575123dbe2cf6e378af18fea0d1
Xianda Sun <sunxdt@gmail.com>
2025-02-27 07:29:38 +0000

    format

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -6,9 +6,7 @@ using JuliaBUGS: phi
 using JuliaBUGS: Bijectors
 using JuliaBUGS.Distributions
 
-examples_to_benchmark = [
-    :rats, :pumps, :oxford, :epil, :lsat, :schools, :beetles, :air
-]
+examples_to_benchmark = [:rats, :pumps, :oxford, :epil, :lsat, :schools, :beetles, :air]
 
 stan_results = Benchmark.benchmark_Stan_models(examples_to_benchmark)
 
@@ -27,7 +25,9 @@ for (i, model_name) in enumerate(examples_to_benchmark)
     model = juliabugs_models[i]
     evaluation_env = deepcopy(model.evaluation_env)
     lowered_model_def = JuliaBUGS._generate_lowered_model_def(model, evaluation_env)
-    expr = JuliaBUGS._gen_log_density_computation_function_expr(lowered_model_def, evaluation_env)
+    expr = JuliaBUGS._gen_log_density_computation_function_expr(
+        lowered_model_def, evaluation_env
+    )
     log_density_eval_function = eval(expr)
     juliabugs_source_gen_mooncake_results[model_name] = Benchmark.benchmark_JuliaBUGS_source_gen(
         model, log_density_eval_function; backend_str=:Mooncake
@@ -40,7 +40,9 @@ for (i, model_name) in enumerate(examples_to_benchmark)
     model = juliabugs_models[i]
     evaluation_env = deepcopy(model.evaluation_env)
     lowered_model_def = JuliaBUGS._generate_lowered_model_def(model, evaluation_env)
-    expr = JuliaBUGS._gen_log_density_computation_function_expr(lowered_model_def, evaluation_env)
+    expr = JuliaBUGS._gen_log_density_computation_function_expr(
+        lowered_model_def, evaluation_env
+    )
     log_density_eval_function = eval(expr)
     try
         juliabugs_source_gen_enzyme_results[model_name] = Benchmark.benchmark_JuliaBUGS_source_gen(
@@ -56,6 +58,8 @@ Benchmark._print_results_table(stan_results; backend=Val(:markdown))
 println("### JuliaBUGS results:")
 Benchmark._print_results_table(juliabugs_results; backend=Val(:markdown))
 println("### JuliaBUGS source gen (Mooncake) results:")
-Benchmark._print_results_table(juliabugs_source_gen_mooncake_results; backend=Val(:markdown))
+Benchmark._print_results_table(
+    juliabugs_source_gen_mooncake_results; backend=Val(:markdown)
+)
 println("### JuliaBUGS source gen (Enzyme) results:")
 Benchmark._print_results_table(juliabugs_source_gen_enzyme_results; backend=Val(:markdown))
