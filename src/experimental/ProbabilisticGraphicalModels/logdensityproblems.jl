389c8de5faf2bf3d4939d4af6af575eadbf9d1c0
Ryan Xiao Wei Lin <rxwl2@cam.ac.uk>
2025-03-03 15:26:13 +0000

    dimension without model (i think there are better ways)

--- src/experimental/ProbabilisticGraphicalModels/logdensityproblems.jl
+++ src/experimental/ProbabilisticGraphicalModels/logdensityproblems.jl
@@ -10,16 +10,39 @@ function LogDensityProblems.logdensity(bn::BayesianNetwork, x::AbstractArray)
 end
 
 """
-    dimension(bn::BayesianNetwork, model::AbstractBUGSModel)
+    dimension(bn::BayesianNetwork)
 
 Return the dimension of the parameter space of the BayesianNetwork.
 """
-function LogDensityProblems.dimension(bn::BayesianNetwork, model::AbstractBUGSModel)
-    return if model.transformed
-        model.transformed_param_length
-    else
-        model.untransformed_param_length
+function LogDensityProblems.dimension(bn::BayesianNetwork)
+    evaluation_env = deepcopy(bn.evaluation_env)
+    
+    # Get all unobserved stochastic variables that need parameters
+    unobserved_stochastic_vars = [
+        bn.names[i] for i in bn.stochastic_ids 
+        if !bn.is_observed[i]
+    ]
+    
+    # Calculate dimensions for each variable
+    total_dim = 0
+    for vn in unobserved_stochastic_vars
+        i = bn.names_to_ids[vn]
+        dist = bn.distributions[i](evaluation_env, bn.loop_vars[vn])
+        
+        if transformed
+            # Calculate transformed dimension
+            b = Bijectors.bijector(dist)
+            var_value = AbstractPPL.get(evaluation_env, vn)
+            transformed_value = Bijectors.transform(b, var_value)
+            total_dim += length(transformed_value)
+        else
+            # Calculate untransformed dimension
+            var_value = AbstractPPL.get(evaluation_env, vn)
+            total_dim += length(var_value)
+        end
     end
+    
+    return total_dim
 end
 
 """
