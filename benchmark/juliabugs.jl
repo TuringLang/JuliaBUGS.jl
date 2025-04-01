852ae32991493ff063c7081fac91ef16c4817c96
Xianda Sun <sunxdt@gmail.com>
2025-04-01 10:43:44 +0100

    only use Mooncake for benchmark

--- benchmark/juliabugs.jl
+++ benchmark/juliabugs.jl
@@ -71,7 +71,9 @@ function _create_JuliaBUGS_model(model_name::Symbol)
     return compile(model_def, data, inits)
 end
 
-function benchmark_JuliaBUGS_model(model::JuliaBUGS.BUGSModel)
+# ! writing a _function_ to benchmark all models won't work because of world-age error
+
+function benchmark_JuliaBUGS_model_with_Mooncake(model::JuliaBUGS.BUGSModel)
     p = Base.Fix1(LogDensityProblems.logdensity, model)
     backend = AutoMooncake(; config=nothing)
     dim = LogDensityProblems.dimension(model)
@@ -82,17 +84,17 @@ function benchmark_JuliaBUGS_model(model::JuliaBUGS.BUGSModel)
     return BenchmarkResult(:juliabugs, dim, density_time, density_and_gradient_time)
 end
 
-function benchmark_JuliaBUGS_model_with_Enzyme(model::JuliaBUGS.BUGSModel)
-    f(params, model) = LogDensityProblems.logdensity(model, params)
-    backend = AutoEnzyme()
-    dim = LogDensityProblems.dimension(model)
-    params_values = JuliaBUGS.getparams(model)
-    prep = prepare_gradient(f, backend, params_values, Constant(model))
-    density_time = Chairmarks.@be LogDensityProblems.logdensity($model, $params_values)
-    density_and_gradient_time = Chairmarks.@be gradient(
-        $f, $prep, $backend, $params_values, $(Constant(model))
-    )
-    return BenchmarkResult(:juliabugs_enzyme, dim, density_time, density_and_gradient_time)
-end
+# function benchmark_JuliaBUGS_model_with_Enzyme(model::JuliaBUGS.BUGSModel)
+#     f(params, model) = LogDensityProblems.logdensity(model, params)
+#     backend = AutoEnzyme()
+#     dim = LogDensityProblems.dimension(model)
+#     params_values = JuliaBUGS.getparams(model)
+#     prep = prepare_gradient(f, backend, params_values, Constant(model))
+#     density_time = Chairmarks.@be LogDensityProblems.logdensity($model, $params_values)
+#     density_and_gradient_time = Chairmarks.@be gradient(
+#         $f, $prep, $backend, $params_values, $(Constant(model))
+#     )
+#     return BenchmarkResult(:juliabugs_enzyme, dim, density_time, density_and_gradient_time)
+# end
 
-# writing a _function_ to benchmark all models won't work because of world-age error
+
--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -14,22 +14,22 @@ juliabugs_models = [
 juliabugs_results = OrderedDict{Symbol,BenchmarkResult}()
 for (model_name, model) in zip(examples_to_benchmark, juliabugs_models)
     @info "Benchmarking $model_name with Mooncake"
-    juliabugs_results[model_name] = benchmark_JuliaBUGS_model(model)
+    juliabugs_results[model_name] = benchmark_JuliaBUGS_model_with_Mooncake(model)
 end
 
-juliabugs_enzyme_results = OrderedDict{Symbol,BenchmarkResult}()
-for (model_name, model) in zip(examples_to_benchmark, juliabugs_models)
-    @info "Benchmarking $model_name with Enzyme"
-    try
-        juliabugs_enzyme_results[model_name] = benchmark_JuliaBUGS_model_with_Enzyme(model)
-    catch e
-        @warn "Error benchmarking $model_name with Enzyme: $e"
-    end
-end
+# juliabugs_enzyme_results = OrderedDict{Symbol,BenchmarkResult}()
+# for (model_name, model) in zip(examples_to_benchmark, juliabugs_models)
+#     @info "Benchmarking $model_name with Enzyme"
+#     try
+#         juliabugs_enzyme_results[model_name] = benchmark_JuliaBUGS_model_with_Enzyme(model)
+#     catch e
+#         @warn "Error benchmarking $model_name with Enzyme: $e"
+#     end
+# end
 
 println("### Stan results:")
 _print_results_table(stan_results; backend=Val(:markdown))
 println("### JuliaBUGS Mooncake results:")
 _print_results_table(juliabugs_results; backend=Val(:markdown))
-println("### JuliaBUGS Enzyme results:")
-_print_results_table(juliabugs_enzyme_results; backend=Val(:markdown))
+# println("### JuliaBUGS Enzyme results:")
+# _print_results_table(juliabugs_enzyme_results; backend=Val(:markdown))
