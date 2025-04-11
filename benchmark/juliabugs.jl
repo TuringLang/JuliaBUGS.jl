1bf61620917f22ea5c861203545561104ec1011f
Xianda Sun <sunxdt@gmail.com>
2025-04-11 09:05:31 +0100

    switch to using generated function, instead of LogDensityProblems, for benchmarking

--- benchmark/juliabugs.jl
+++ benchmark/juliabugs.jl
@@ -74,12 +74,13 @@ end
 # ! writing a _function_ to benchmark all models won't work because of world-age error
 
 function benchmark_JuliaBUGS_model_with_Mooncake(model::JuliaBUGS.BUGSModel)
-    p = Base.Fix1(LogDensityProblems.logdensity, model)
+    # p = Base.Fix1(LogDensityProblems.logdensity, model)
+    p = Base.Fix1(model.log_density_computation_function, model.evaluation_env)
     backend = AutoMooncake(; config=nothing)
     dim = LogDensityProblems.dimension(model)
     params_values = JuliaBUGS.getparams(model)
     prep = prepare_gradient(p, backend, params_values)
-    density_time = Chairmarks.@be LogDensityProblems.logdensity($model, $params_values)
+    density_time = Chairmarks.@be $p($params_values)
     density_and_gradient_time = Chairmarks.@be gradient($p, $prep, $backend, $params_values)
     return BenchmarkResult(:juliabugs, dim, density_time, density_and_gradient_time)
 end
