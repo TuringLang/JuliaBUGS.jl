2fc6a20093fad7ebe2a13865143bc4cfc8e13b96
Xianda Sun <5433119+sunxd3@users.noreply.github.com>
2025-04-15 09:31:14 +0100

    Update test/source_gen.jl
    Co-authored-by: Penelope Yong <penelopeysm@gmail.com>

--- test/source_gen.jl
+++ test/source_gen.jl
@@ -56,35 +56,26 @@ function _create_bugsmodel_with_consistent_sorted_nodes(
     return new_model
 end
 
-bugs_models = OrderedDict{Symbol,JuliaBUGS.BUGSModel}()
-evaluation_envs = OrderedDict{Symbol,NamedTuple}()
-log_density_computation_functions = OrderedDict{Symbol,Function}()
-reconstructed_model_defs = OrderedDict{Symbol,Expr}()
-for example_name in test_examples
+@testset "source_gen: $example_name" for example_name in test_examples
     model, evaluation_env = _create_model(example_name)
-    bugs_models[example_name] = model
-    evaluation_envs[example_name] = evaluation_env
     lowered_model_def, reconstructed_model_def = _generate_lowered_model_def(
         model.model_def, model.g, evaluation_env
     )
     log_density_computation_expr = _gen_log_density_computation_function_expr(
         lowered_model_def, evaluation_env, gensym(example_name)
     )
-    log_density_computation_functions[example_name] = eval(log_density_computation_expr)
-    reconstructed_model_defs[example_name] = reconstructed_model_def
-end
+    log_density_computation_function = eval(log_density_computation_expr)
 
-@testset "source_gen: $example_name" for example_name in test_examples
     model_with_consistent_sorted_nodes = _create_bugsmodel_with_consistent_sorted_nodes(
-        bugs_models[example_name], reconstructed_model_defs[example_name]
+        model, reconstructed_model_def
     )
-    result_with_old_model = JuliaBUGS.evaluate!!(bugs_models[example_name])[2]
+    result_with_old_model = JuliaBUGS.evaluate!!(model)[2]
     params = JuliaBUGS.getparams(model_with_consistent_sorted_nodes)
     result_with_bugsmodel = JuliaBUGS.evaluate!!(
         model_with_consistent_sorted_nodes, params
     )[2]
-    result_with_log_density_computation_function = log_density_computation_functions[example_name](
-        evaluation_envs[example_name], params
+    result_with_log_density_computation_function = log_density_computation_function(
+        evaluation_env, params
     )
     @test result_with_old_model ≈ result_with_bugsmodel
     @test result_with_log_density_computation_function ≈ result_with_bugsmodel
