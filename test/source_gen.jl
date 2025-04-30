056e6ec4039ceea3c22c7a9e67382340d928fecf
Xianda Sun <sunxdt@gmail.com>
2025-04-30 14:41:16 +0100

    fix test

--- test/source_gen.jl
+++ test/source_gen.jl
@@ -35,50 +35,18 @@ test_examples = [
     :cervix,
 ]
 
-function _create_model(model_name::Symbol)
-    (; model_def, data, inits) = getfield(JuliaBUGS.BUGSExamples, model_name)
-    model = compile(model_def, data, inits)
-    evaluation_env = deepcopy(model.evaluation_env)
-    return model, evaluation_env
-end
-
-function _create_bugsmodel_with_consistent_sorted_nodes(
-    model::JuliaBUGS.BUGSModel, reconstructed_model_def
-)
-    pass = CollectSortedNodes(model.evaluation_env)
-    JuliaBUGS.analyze_block(pass, reconstructed_model_def)
-    sorted_nodes = pass.sorted_nodes
-    sorted_parameters = [vn for vn in sorted_nodes if vn in model.parameters]
-    new_flattened_graph_node_data = JuliaBUGS.FlattenedGraphNodeData(model.g, sorted_nodes)
-    new_model = BangBang.setproperty!!(model, :parameters, sorted_parameters)
-    new_model = BangBang.setproperty!!(
-        new_model, :flattened_graph_node_data, new_flattened_graph_node_data
-    )
-    return new_model
-end
-
 @testset "source_gen: $example_name" for example_name in test_examples
-    model, evaluation_env = _create_model(example_name)
-    lowered_model_def, reconstructed_model_def = _generate_lowered_model_def(
-        model.model_def, model.g, evaluation_env
-    )
-    log_density_computation_expr = _gen_log_density_computation_function_expr(
-        lowered_model_def, evaluation_env, gensym(example_name)
-    )
-    log_density_computation_function = eval(log_density_computation_expr)
-
-    model_with_consistent_sorted_nodes = _create_bugsmodel_with_consistent_sorted_nodes(
-        model, reconstructed_model_def
-    )
-    result_with_old_model = JuliaBUGS.evaluate!!(model)[2]
-    params = JuliaBUGS.getparams(model_with_consistent_sorted_nodes)
-    result_with_bugsmodel = JuliaBUGS.evaluate!!(
-        model_with_consistent_sorted_nodes, params
-    )[2]
-    result_with_log_density_computation_function = log_density_computation_function(
-        evaluation_env, params
-    )
-    @test result_with_old_model ≈ result_with_bugsmodel
+    (; model_def, data, inits) = getfield(JuliaBUGS.BUGSExamples, example_name)
+    model = compile(model_def, data, inits)
+    params = JuliaBUGS.getparams(model)
+    result_with_bugsmodel = begin
+        model = JuliaBUGS.set_evaluation_mode(model, JuliaBUGS.UseGraph())
+        LogDensityProblems.logdensity(model, params)
+    end
+    result_with_log_density_computation_function = begin
+        model = JuliaBUGS.set_evaluation_mode(model, JuliaBUGS.UseGeneratedLogDensityFunction())
+        LogDensityProblems.logdensity(model, params)
+    end
     @test result_with_log_density_computation_function ≈ result_with_bugsmodel
 end
 
