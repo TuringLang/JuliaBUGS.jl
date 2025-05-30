05c625501982f0c72d05bb3f85f11a36e1254918
Ryan Xiao Wei Lin <rxwl2@cam.ac.uk>
2025-05-30 10:32:47 +0100

    dimension for  only continuous parameter

--- src/experimental/ProbabilisticGraphicalModels/logdensityproblems.jl
+++ src/experimental/ProbabilisticGraphicalModels/logdensityproblems.jl
@@ -4,8 +4,8 @@
 Compute the log density of the BayesianNetwork given parameters x.
 Requires the original BUGSModel to access transformed_var_lengths.
 """
+
 function LogDensityProblems.logdensity(bn::BayesianNetwork, x::AbstractArray)
-    println("LogDensityProblems.logdensity calling 1st function")
     _, logp = evaluate_with_marginalization(bn, x)
     return logp
 end
@@ -16,9 +16,8 @@ end
 Return the dimension of the parameter space of the BayesianNetwork.
 """
 function LogDensityProblems.dimension(bn::BayesianNetwork)
-    return bn.transformed_param_length - 1
+    return sum(bn.is_stochastic .& (.!bn.is_observed) .& (bn.node_types .!= :discrete))
 end
-
 """
     capabilities(::BayesianNetwork)
 
@@ -27,3 +26,4 @@ Return the differentiation capabilities of the BayesianNetwork.
 function LogDensityProblems.capabilities(::BayesianNetwork)
     return LogDensityProblems.LogDensityOrder{0}()
 end
+
