0234f7a8b5a30b372658d5b9a8c8e7cf75f07459
Xianda Sun <5433119+sunxd3@users.noreply.github.com>
2025-06-01 14:00:08 +0100

    Update src/model/abstractppl.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/model/abstractppl.jl
+++ src/model/abstractppl.jl
@@ -415,7 +415,9 @@ function decondition(model::BUGSModel, vars_to_decondition::Vector{<:VarName})
 
     # Add deconditioned variables back to parameters
     # Need to maintain topological order
-    new_parameters_unsorted = vcat(model.graph_evaluation_data.sorted_parameters, expanded_vars)
+    new_parameters_unsorted = vcat(
+        model.graph_evaluation_data.sorted_parameters, expanded_vars
+    )
     new_parameters = [
         vn for vn in new_graph_evaluation_data.sorted_nodes if vn in new_parameters_unsorted
     ]
