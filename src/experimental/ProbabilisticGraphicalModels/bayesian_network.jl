b76bd2a9417979632da7c0075e4c91427dc14f8b
Ryan Xiao Wei Lin <rxwl2@cam.ac.uk>
2025-06-09 20:28:37 +0100

    linting

--- src/experimental/ProbabilisticGraphicalModels/bayesian_network.jl
+++ src/experimental/ProbabilisticGraphicalModels/bayesian_network.jl
@@ -30,22 +30,22 @@ struct BayesianNetwork{V,T,F}
 end
 
 function BayesianNetwork{V}() where {V}
-	return BayesianNetwork(
-		SimpleDiGraph{Int}(), # by default, vertex ids are integers
-		V[],
-		Dict{V, Int}(),
-		(;),    # Empty NamedTuple for evaluation_env
-		Dict{V, NamedTuple}(),
-		Any[],
-		Any[],
-		Int[],
-		Int[],
-		BitVector(),
-		BitVector(),
-		Symbol[],
-		Dict{V, Int}(),  # Empty Dict for transformed_var_lengths
-		0,              # transformed_param_length
-	)
+    return BayesianNetwork(
+        SimpleDiGraph{Int}(), # by default, vertex ids are integers
+        V[],
+        Dict{V,Int}(),
+        (;),    # Empty NamedTuple for evaluation_env
+        Dict{V,NamedTuple}(),
+        Any[],
+        Any[],
+        Int[],
+        Int[],
+        BitVector(),
+        BitVector(),
+        Symbol[],
+        Dict{V,Int}(),  # Empty Dict for transformed_var_lengths
+        0,              # transformed_param_length
+    )
 end
 
 """
@@ -54,82 +54,82 @@ end
 Translates a BUGSGraph (with node metadata stored in NodeInfo) into a BayesianNetwork.
 """
 function translate_BUGSGraph_to_BayesianNetwork(
-	g::JuliaBUGS.BUGSGraph, evaluation_env, model = nothing,
+    g::JuliaBUGS.BUGSGraph, evaluation_env, model=nothing
 )
-	# Retrieve variable labels (stored as VarNames) from g.
-	varnames = collect(labels(g))
-	n = length(varnames)
-	original_graph = g.graph
+    # Retrieve variable labels (stored as VarNames) from g.
+    varnames = collect(labels(g))
+    n = length(varnames)
+    original_graph = g.graph
 
-	# Preallocate arrays/dictionaries.
-	names = Vector{VarName}(undef, n)
-	names_to_ids = Dict{VarName, Int}()
-	loop_vars = Dict{VarName, NamedTuple}()
-	distributions = Vector{Function}(undef, n)
-	deterministic_fns = Vector{Function}(undef, n)
-	stochastic_ids = Int[]
-	deterministic_ids = Int[]
-	is_stochastic = falses(n)
-	is_observed = falses(n)
-	node_types = Vector{Symbol}(undef, n)
-	transformed_var_lengths = Dict{VarName, Int}()
-	transformed_param_length = 0
+    # Preallocate arrays/dictionaries.
+    names = Vector{VarName}(undef, n)
+    names_to_ids = Dict{VarName,Int}()
+    loop_vars = Dict{VarName,NamedTuple}()
+    distributions = Vector{Function}(undef, n)
+    deterministic_fns = Vector{Function}(undef, n)
+    stochastic_ids = Int[]
+    deterministic_ids = Int[]
+    is_stochastic = falses(n)
+    is_observed = falses(n)
+    node_types = Vector{Symbol}(undef, n)
+    transformed_var_lengths = Dict{VarName,Int}()
+    transformed_param_length = 0
 
-	if model !== nothing
-		if isdefined(model, :transformed_var_lengths)
-			transformed_var_lengths = copy(model.transformed_var_lengths)
-		end
-		if isdefined(model, :transformed_param_length)
-			transformed_param_length = model.transformed_param_length
-		end
-	end
+    if model !== nothing
+        if isdefined(model, :transformed_var_lengths)
+            transformed_var_lengths = copy(model.transformed_var_lengths)
+        end
+        if isdefined(model, :transformed_param_length)
+            transformed_param_length = model.transformed_param_length
+        end
+    end
 
-	for (i, varname) in enumerate(varnames)
-		nodeinfo = g[varname]
-		names[i] = varname
-		names_to_ids[varname] = i
-		is_stochastic[i] = nodeinfo.is_stochastic
-		is_observed[i] = nodeinfo.is_observed
-		loop_vars[varname] = nodeinfo.loop_vars
+    for (i, varname) in enumerate(varnames)
+        nodeinfo = g[varname]
+        names[i] = varname
+        names_to_ids[varname] = i
+        is_stochastic[i] = nodeinfo.is_stochastic
+        is_observed[i] = nodeinfo.is_observed
+        loop_vars[varname] = nodeinfo.loop_vars
 
-		if nodeinfo.is_stochastic
-			distributions[i] = nodeinfo.node_function
-			push!(stochastic_ids, i)
-			node_types[i] = :stochastic
-		else
-			deterministic_fns[i] = nodeinfo.node_function
-			push!(deterministic_ids, i)
-			node_types[i] = :deterministic
-		end
-	end
+        if nodeinfo.is_stochastic
+            distributions[i] = nodeinfo.node_function
+            push!(stochastic_ids, i)
+            node_types[i] = :stochastic
+        else
+            deterministic_fns[i] = nodeinfo.node_function
+            push!(deterministic_ids, i)
+            node_types[i] = :deterministic
+        end
+    end
 
-	bn = BayesianNetwork(
-		SimpleDiGraph{Int}(n),
-		names,
-		names_to_ids,
-		evaluation_env,
-		loop_vars,
-		distributions,
-		deterministic_fns,
-		stochastic_ids,
-		deterministic_ids,
-		is_stochastic,
-		is_observed,
-		node_types,
-		transformed_var_lengths,
-		transformed_param_length,
-	)
+    bn = BayesianNetwork(
+        SimpleDiGraph{Int}(n),
+        names,
+        names_to_ids,
+        evaluation_env,
+        loop_vars,
+        distributions,
+        deterministic_fns,
+        stochastic_ids,
+        deterministic_ids,
+        is_stochastic,
+        is_observed,
+        node_types,
+        transformed_var_lengths,
+        transformed_param_length,
+    )
 
-	# Add edges using the BayesianNetwork's mapping.
-	for e in edges(original_graph)
-		let src_name = bn.names[e.src]
-			let dst_name = bn.names[e.dst]
-				add_edge!(bn, src_name, dst_name)
-			end
-		end
-	end
+    # Add edges using the BayesianNetwork's mapping.
+    for e in edges(original_graph)
+        let src_name = bn.names[e.src]
+            let dst_name = bn.names[e.dst]
+                add_edge!(bn, src_name, dst_name)
+            end
+        end
+    end
 
-	return bn
+    return bn
 end
 
 """
@@ -139,22 +139,22 @@ Add a stochastic vertex with name `name`, a distribution object/function `dist`,
 and a declared node_type (`:discrete` or `:continuous`).
 """
 function add_stochastic_vertex!(
-	bn::BayesianNetwork{V, T},
-	name::V,
-	dist::Any,
-	is_observed::Bool = false,
-	node_type::Symbol = :continuous,
-)::T where {V, T}
-	Graphs.add_vertex!(bn.graph) || return 0
-	id = nv(bn.graph)
-	push!(bn.distributions, dist)
-	push!(bn.is_stochastic, true)
-	push!(bn.is_observed, is_observed)
-	push!(bn.names, name)
-	bn.names_to_ids[name] = id
-	push!(bn.stochastic_ids, id)
-	push!(bn.node_types, node_type)
-	return id
+    bn::BayesianNetwork{V,T},
+    name::V,
+    dist::Any,
+    is_observed::Bool=false,
+    node_type::Symbol=:continuous,
+)::T where {V,T}
+    Graphs.add_vertex!(bn.graph) || return 0
+    id = nv(bn.graph)
+    push!(bn.distributions, dist)
+    push!(bn.is_stochastic, true)
+    push!(bn.is_observed, is_observed)
+    push!(bn.names, name)
+    bn.names_to_ids[name] = id
+    push!(bn.stochastic_ids, id)
+    push!(bn.node_types, node_type)
+    return id
 end
 
 """
@@ -162,17 +162,17 @@ end
 
 Add a deterministic vertex.
 """
-function add_deterministic_vertex!(bn::BayesianNetwork{V, T}, name::V, f::F)::T where {T, V, F}
-	Graphs.add_vertex!(bn.graph) || return 0
-	id = nv(bn.graph)
-	push!(bn.deterministic_functions, f)
-	push!(bn.is_stochastic, false)
-	push!(bn.is_observed, false)
-	push!(bn.names, name)
-	bn.names_to_ids[name] = id
-	push!(bn.deterministic_ids, id)
-	push!(bn.node_types, :deterministic)
-	return id
+function add_deterministic_vertex!(bn::BayesianNetwork{V,T}, name::V, f::F)::T where {T,V,F}
+    Graphs.add_vertex!(bn.graph) || return 0
+    id = nv(bn.graph)
+    push!(bn.deterministic_functions, f)
+    push!(bn.is_stochastic, false)
+    push!(bn.is_observed, false)
+    push!(bn.names, name)
+    bn.names_to_ids[name] = id
+    push!(bn.deterministic_ids, id)
+    push!(bn.node_types, :deterministic)
+    return id
 end
 
 """
@@ -180,84 +180,84 @@ end
 
 Add a directed edge from `from` -> `to`.
 """
-function add_edge!(bn::BayesianNetwork{V, T}, from::V, to::V)::Bool where {T, V}
-	from_id = bn.names_to_ids[from]
-	to_id = bn.names_to_ids[to]
-	return Graphs.add_edge!(bn.graph, from_id, to_id)
+function add_edge!(bn::BayesianNetwork{V,T}, from::V, to::V)::Bool where {T,V}
+    from_id = bn.names_to_ids[from]
+    to_id = bn.names_to_ids[to]
+    return Graphs.add_edge!(bn.graph, from_id, to_id)
 end
 
 function evaluate(bn::BayesianNetwork)
-	logp = 0.0
-	evaluation_env = bn.evaluation_env
+    logp = 0.0
+    evaluation_env = bn.evaluation_env
 
-	for (i, varname) in enumerate(bn.names)
-		is_stochastic = bn.is_stochastic[i]
-		if is_stochastic
-			dist_fn = bn.distributions[i](evaluation_env, bn.loop_vars[varname])
+    for (i, varname) in enumerate(bn.names)
+        is_stochastic = bn.is_stochastic[i]
+        if is_stochastic
+            dist_fn = bn.distributions[i](evaluation_env, bn.loop_vars[varname])
 
-			value = AbstractPPL.get(evaluation_env, varname)
-			bijector = Bijectors.bijector(dist_fn)
-			value_transformed = Bijectors.transform(bijector, value)
+            value = AbstractPPL.get(evaluation_env, varname)
+            bijector = Bijectors.bijector(dist_fn)
+            value_transformed = Bijectors.transform(bijector, value)
 
-			logpdf_val = Distributions.logpdf(dist_fn, value)
-			logjac = Bijectors.logabsdetjac(Bijectors.inverse(bijector), value_transformed)
-			logp += logpdf_val + logjac
+            logpdf_val = Distributions.logpdf(dist_fn, value)
+            logjac = Bijectors.logabsdetjac(Bijectors.inverse(bijector), value_transformed)
+            logp += logpdf_val + logjac
 
-		else
-			fn = bn.deterministic_functions[i](evaluation_env, bn.loop_vars[varname])
-			evaluation_env = BangBang.setindex!!(evaluation_env, fn, varname)
-		end
-	end
-	return evaluation_env, logp
+        else
+            fn = bn.deterministic_functions[i](evaluation_env, bn.loop_vars[varname])
+            evaluation_env = BangBang.setindex!!(evaluation_env, fn, varname)
+        end
+    end
+    return evaluation_env, logp
 end
 
 function evaluate_with_values(bn::BayesianNetwork, parameter_values::AbstractVector)
-	bugsmodel_node_order = [bn.names[i] for i in topological_sort_by_dfs(bn.graph)]
-	var_lengths = bn.transformed_var_lengths
+    bugsmodel_node_order = [bn.names[i] for i in topological_sort_by_dfs(bn.graph)]
+    var_lengths = bn.transformed_var_lengths
 
-	evaluation_env = deepcopy(bn.evaluation_env)
-	current_idx = 1
-	logprior, loglikelihood = 0.0, 0.0
+    evaluation_env = deepcopy(bn.evaluation_env)
+    current_idx = 1
+    logprior, loglikelihood = 0.0, 0.0
 
-	for vn in bugsmodel_node_order
-		i = bn.names_to_ids[vn]
+    for vn in bugsmodel_node_order
+        i = bn.names_to_ids[vn]
 
-		is_stochastic = bn.is_stochastic[i]
-		is_observed = bn.is_observed[i]
+        is_stochastic = bn.is_stochastic[i]
+        is_observed = bn.is_observed[i]
 
-		if !is_stochastic
-			value = bn.deterministic_functions[i](evaluation_env, bn.loop_vars[vn])
-			evaluation_env = BangBang.setindex!!(evaluation_env, value, vn)
-		else
-			if !is_observed
-				dist = bn.distributions[i](evaluation_env, bn.loop_vars[vn])
-				b = Bijectors.bijector(dist)
-				# If the variable is not in transformed_var_lengths, calculate it
-				if !haskey(var_lengths, vn)
-					var_value = AbstractPPL.get(evaluation_env, vn)
-					transformed_value = Bijectors.transform(b, var_value)
-					var_lengths[vn] = length(transformed_value)
-				end
-				l = var_lengths[vn]
-				b_inv = Bijectors.inverse(b)
-				reconstructed_value = JuliaBUGS.reconstruct(
-					b_inv, dist, view(parameter_values, current_idx:(current_idx+l-1)),
-				)
-				value, logjac = Bijectors.with_logabsdet_jacobian(
-					b_inv, reconstructed_value,
-				)
+        if !is_stochastic
+            value = bn.deterministic_functions[i](evaluation_env, bn.loop_vars[vn])
+            evaluation_env = BangBang.setindex!!(evaluation_env, value, vn)
+        else
+            if !is_observed
+                dist = bn.distributions[i](evaluation_env, bn.loop_vars[vn])
+                b = Bijectors.bijector(dist)
+                # If the variable is not in transformed_var_lengths, calculate it
+                if !haskey(var_lengths, vn)
+                    var_value = AbstractPPL.get(evaluation_env, vn)
+                    transformed_value = Bijectors.transform(b, var_value)
+                    var_lengths[vn] = length(transformed_value)
+                end
+                l = var_lengths[vn]
+                b_inv = Bijectors.inverse(b)
+                reconstructed_value = JuliaBUGS.reconstruct(
+                    b_inv, dist, view(parameter_values, current_idx:(current_idx + l - 1))
+                )
+                value, logjac = Bijectors.with_logabsdet_jacobian(
+                    b_inv, reconstructed_value
+                )
 
-				current_idx += l
-				logprior += logpdf(dist, value) + logjac
-				evaluation_env = BangBang.setindex!!(evaluation_env, value, vn)
-			else
-				dist = bn.distributions[i](evaluation_env, bn.loop_vars[vn])
-				loglikelihood += logpdf(dist, AbstractPPL.get(evaluation_env, vn))
-			end
-		end
-	end
+                current_idx += l
+                logprior += logpdf(dist, value) + logjac
+                evaluation_env = BangBang.setindex!!(evaluation_env, value, vn)
+            else
+                dist = bn.distributions[i](evaluation_env, bn.loop_vars[vn])
+                loglikelihood += logpdf(dist, AbstractPPL.get(evaluation_env, vn))
+            end
+        end
+    end
 
-	return evaluation_env, logprior + loglikelihood
+    return evaluation_env, logprior + loglikelihood
 end
 
 """
@@ -265,87 +265,87 @@ This function works in some cases, but not all. I have yet to discover why.
 	In some functions, we have to hash the whole environment to get the correct answer.
 """
 function _extract_parent_values(bn::BayesianNetwork, node_id::Int, env)
-	# Get the parents (incoming neighbors) of this node
-	parent_ids = inneighbors(bn.graph, node_id)
+    # Get the parents (incoming neighbors) of this node
+    parent_ids = inneighbors(bn.graph, node_id)
 
-	# Initialize dictionary allowing both Int and Symbol keys 
-	parent_values = Dict{Union{Int, Symbol}, Any}()
+    # Initialize dictionary allowing both Int and Symbol keys 
+    parent_values = Dict{Union{Int,Symbol},Any}()
 
-	# If no parents, just return the empty dictionary
-	if isempty(parent_ids)
-		# We still need to potentially add metadata
-		# No need for an early return
-	end
+    # If no parents, just return the empty dictionary
+    if isempty(parent_ids)
+        # We still need to potentially add metadata
+        # No need for an early return
+    end
 
-	# Add parent values to the dictionary
-	for pid in parent_ids
-		parent_name = bn.names[pid]
+    # Add parent values to the dictionary
+    for pid in parent_ids
+        parent_name = bn.names[pid]
 
-		try
-			value = AbstractPPL.get(env, parent_name)
-			parent_values[pid] = value
-		catch e
-			if isa(e, KeyError) || isa(e, MethodError)
-				parent_values[pid] = :__MISSING__
-			else
-				rethrow(e)
-			end
-		end
-	end
+        try
+            value = AbstractPPL.get(env, parent_name)
+            parent_values[pid] = value
+        catch e
+            if isa(e, KeyError) || isa(e, MethodError)
+                parent_values[pid] = :__MISSING__
+            else
+                rethrow(e)
+            end
+        end
+    end
 
-	# Add observation state to metadata
-	if bn.is_stochastic[node_id] && bn.is_observed[node_id]
-		parent_values[:__OBSERVED__] = true
-	end
+    # Add observation state to metadata
+    if bn.is_stochastic[node_id] && bn.is_observed[node_id]
+        parent_values[:__OBSERVED__] = true
+    end
 
-	return parent_values
+    return parent_values
 end
 
 # Extract only discrete variable values from the environment
 function _extract_discrete_values(bn::BayesianNetwork, env)
-	discrete_values = Dict{Union{Int, Symbol}, Any}()
+    discrete_values = Dict{Union{Int,Symbol},Any}()
 
-	for i in 1:length(bn.names)
-		if bn.node_types[i] == :discrete
-			name = bn.names[i]
-			try
-				discrete_values[i] = AbstractPPL.get(env, name)
-			catch
-				discrete_values[i] = :__MISSING__
-			end
-		end
-	end
+    for i in 1:length(bn.names)
+        if bn.node_types[i] == :discrete
+            name = bn.names[i]
+            try
+                discrete_values[i] = AbstractPPL.get(env, name)
+            catch
+                discrete_values[i] = :__MISSING__
+            end
+        end
+    end
 
-	return discrete_values
+    return discrete_values
 end
 
 function _precompute_minimal_cache_keys(bn, order::Vector{Int})
-	minimal_keys = Dict{Int, Set{Int}}()
-	n = length(order)
+    minimal_keys = Dict{Int,Set{Int}}()
+    n = length(order)
 
-	for t in 1:n
-		current_node_id = order[t]
-		future_nodes = t < n ? order[(t+1):end] : Int[]
+    for t in 1:n
+        current_node_id = order[t]
+        future_nodes = t < n ? order[(t + 1):end] : Int[]
 
-		# Collect parents of future nodes + current node's parents
-		future_parents = Set{Int}()
-		union!(future_parents, inneighbors(bn.graph, current_node_id))
-		for n in future_nodes
-			union!(future_parents, inneighbors(bn.graph, n))
-		end
+        # Collect parents of future nodes + current node's parents
+        future_parents = Set{Int}()
+        union!(future_parents, inneighbors(bn.graph, current_node_id))
+        for n in future_nodes
+            union!(future_parents, inneighbors(bn.graph, n))
+        end
 
-		# Get visited nodes up to current position
-		visited = Set(order[1:t])
+        # Get visited nodes up to current position
+        visited = Set(order[1:t])
 
-		# Store by node ID
-		minimal_keys[current_node_id] = intersect(future_parents, visited)
-	end
+        # Store by node ID
+        minimal_keys[current_node_id] = intersect(future_parents, visited)
+    end
 
-	return minimal_keys
+    return minimal_keys
 end
 
 function _precompute_minimal_cache_keys(bn)
-	return _precompute_minimal_cache_keys(bn, topological_sort_by_dfs(bn.graph))
+    return _precompute_minimal_cache_keys(bn, topological_sort_by_dfs(bn.graph))
 end
 
 """
