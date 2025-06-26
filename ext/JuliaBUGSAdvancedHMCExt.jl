68a49475dd3a136e9203095887519146c776ac91
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-06-26 19:31:22 +0530

    Update JuliaBUGSAdvancedHMCExt.jl

--- ext/JuliaBUGSAdvancedHMCExt.jl
+++ ext/JuliaBUGSAdvancedHMCExt.jl
@@ -24,15 +24,20 @@ function AbstractMCMC.bundle_samples(
     thinning=1,
     kwargs...,
 )
+    using DynamicPPL: get_transform_info, invlink
+
     stats_names = collect(keys(merge((; lp=ts[1].z.ℓπ.value), AdvancedHMC.stat(ts[1]))))
     stats_values = [
         vcat([ts[i].z.ℓπ.value..., collect(values(AdvancedHMC.stat(ts[i])))...]) for
         i in eachindex(ts)
     ]
 
+    trans = get_transform_info(logdensitymodel.logdensity)
+    param_vals = [invlink(trans, t.z) for t in ts]
+
     return JuliaBUGS.gen_chains(
         logdensitymodel,
-        [t.z.θ for t in ts],
+        param_vals,
         stats_names,
         stats_values;
         discard_initial=discard_initial,
@@ -62,4 +67,46 @@ function JuliaBUGS.gibbs_internal(
     )
 end
 
+function AbstractMCMC.step(
+    rng::AbstractRNG,
+    model::AbstractBUGSModel,
+    sampler::HMC;
+    n_adapts=0,
+    initial_params=nothing,
+    kwargs...
+)
+    logdensitymodel = AbstractMCMC.LogDensityModel(
+        LogDensityProblemsAD.ADgradient(:ReverseDiff, model)
+    )
+    t, s = AbstractMCMC.step(
+        rng,
+        logdensitymodel,
+        sampler;
+        n_adapts=n_adapts,
+        initial_params=initial_params,
+        kwargs...
+    )
+
+    vi = JuliaBUGS.varinfo_from_θ(model, t.z.θ)
+    ctx = LogDensityContext()
+    _, vi_gen = evaluate!!(model, ctx, vi)
+
+    all_values = Dict{Symbol, Any}()
+    for (name, val) in JuliaBUGS.getparams(vi_gen)
+        if should_save(name, model)
+            all_values[name] = val
+        end
+    end
+
+    return all_values, s
 end
+
+function should_save(name::Symbol, model::AbstractBUGSModel)
+    if hasproperty(model, :saved_variables) && model.saved_variables !== nothing
+        return name in model.saved_variables
+    end
+    return true
+end
+
+end
+
