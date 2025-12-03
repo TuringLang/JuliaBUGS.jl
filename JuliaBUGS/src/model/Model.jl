fff9c65d9fb621d27461a1900e65eaafc4e4d081
Xianda Sun <sunxdt@gmail.com>
2025-12-03 10:21:43 +0000

    Export internal evaluation functions from Model for test access

--- JuliaBUGS/src/model/Model.jl
+++ JuliaBUGS/src/model/Model.jl
@@ -26,4 +26,9 @@ export set_evaluation_mode, set_observed_values!
 # Evaluation mode types
 export UseGraph, UseGeneratedLogDensityFunction, UseAutoMarginalization
 
+# Internal evaluation functions (exported for testing, not re-exported to users)
+export evaluate_with_rng!!, evaluate_with_env!!, evaluate_with_values!!
+export evaluate_with_marginalization_rng!!,
+    evaluate_with_marginalization_env!!, evaluate_with_marginalization_values!!
+
 end # Model
