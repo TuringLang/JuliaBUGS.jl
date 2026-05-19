7af1e8f68740e80750067b21607ce7010a8cd017
Shravan Goswami <shravanngoswamii@gmail.com>
2026-05-20 00:22:19 +0530

    Run AD compatibility coverage with log density tests

--- JuliaBUGS/test/runtests.jl
+++ JuliaBUGS/test/runtests.jl
@@ -92,6 +92,7 @@ const TEST_GROUPS = OrderedDict{String,Function}(
         include("model/auto_marginalization.jl")
         include("model/frontier_cache_hmm.jl")
         include("model/domain_error_handling.jl")
+        include("ad_compatibility.jl")
     end,
     "inference" => () -> begin
         include("independent_mh.jl")
